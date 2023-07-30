6c9b38af567e72139d3384d67dd4e5496f37949a
Sergey Kosarevsky <sk@linderdaum.com>
2023-07-29 18:17:05 -0700

    Patch CMake versions in third-party dependencies to fix CMake deprecation warnings

---
+++ third-party/deps/patches/meshoptimizer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import os
+import shutil
+from pathlib import Path
+
+base_dir=os.getcwd()
+lib_dir=os.path.join(base_dir, "src", "meshoptimizer")
+
+file = Path(os.path.join(lib_dir, "CMakeLists.txt"))
+file.write_text(file.read_text()
+  .replace('VERSION 3.0', 'VERSION 3.5'))
