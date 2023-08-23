c1b6dc4fffaf41b8f0fd3ea518f7cc7b7d5f466b
Mauricio Maurer <mmaurer@meta.com>
2023-08-23 14:31:37 -0700

    Use `IGL_VERIFY` to enable the buffer device address extension
    Summary: Using `IGL_VERIFY` instead of `IGL_ASSERT` ensures that the extension is enabled in all build modes (debug, release, etc).
    
    Reviewed By: corporateshark
    
    Differential Revision: D48606761
    
    fbshipit-source-id: 6056491c6544d2b3fe10ae89740b341ddb634491

--- src/igl/vulkan/VulkanContext.cpp
+++ src/igl/vulkan/VulkanContext.cpp
@@ -454,8 +454,8 @@ igl::Result VulkanContext::initContext(const HWDeviceDesc& desc,
     extensions_.enable(extraDeviceExtensions[i], VulkanExtensions::ExtensionType::Device);
   }
   if (config_.enableBufferDeviceAddress) {
-    IGL_ASSERT(extensions_.enable(VK_KHR_BUFFER_DEVICE_ADDRESS_EXTENSION_NAME,
-                                  VulkanExtensions::ExtensionType::Device));
+    (void)IGL_VERIFY(extensions_.enable(VK_KHR_BUFFER_DEVICE_ADDRESS_EXTENSION_NAME,
+                                        VulkanExtensions::ExtensionType::Device));
   }
 
   VulkanQueuePool queuePool(vkPhysicalDevice_);
