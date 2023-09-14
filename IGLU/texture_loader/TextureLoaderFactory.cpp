1d52a64c006a7e55ddc8690c57b8d329c8781e76
Eric Griffith <ericgriffith@meta.com>
2023-09-14 13:17:31 -0700

    IGL: Add meta texture loader factory
    Summary:
    This diff adds a new, meta texture loader factory that wraps other texture loader factories.
    
    The use case for this is generic texture loading.
     1) Callers will generally have access to a buffer of binary data, but may not always have access to the metadata needed to determine which specific format the data is in. With this factory, the caller can just ask if the factory can create a texture loader for it, and, internally this factory will ask all of the factories it wraps if they can create a loader for the data.
     2) Even when callers do have the necessary metadata to know which format is being loaded, with this approach they don't need a bunch of conditional logic at the place where they want to load the texture to determine which loader to use. The conditional logic is pushed inside this factory.
     3) This allows texture loading support to be set up in a central place, after which callers only need access to this single factory to load textures. This makes it easier to set up different configurations for the central texture loading depending on things like app configuration without needing to propagate those changes throughout the app.
    
    Reviewed By: syeh1
    
    Differential Revision: D49135941
    
    fbshipit-source-id: dec97bad083ef20e358984c2723dec385b19677b

---
+++ IGLU/texture_loader/TextureLoaderFactory.cpp
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <IGLU/texture_loader/TextureLoaderFactory.h>
+
+namespace iglu::textureloader {
+
+TextureLoaderFactory::TextureLoaderFactory(
+    std::vector<std::unique_ptr<ITextureLoaderFactory>>&& factories) :
+  factories_(std::move(factories)), headerLength_(0) {
+  for (const auto& factory : factories_) {
+    headerLength_ = std::max(headerLength_, factory->headerLength());
+  }
+}
+
+uint32_t TextureLoaderFactory::headerLength() const noexcept {
+  return headerLength_;
+}
+
+bool TextureLoaderFactory::canCreateInternal(DataReader headerReader,
+                                             igl::Result* IGL_NULLABLE outResult) const noexcept {
+  for (const auto& factory : factories_) {
+    if (factory->canCreate(headerReader, nullptr)) {
+      return true;
+    }
+  }
+
+  igl::Result::setResult(outResult, igl::Result::Code::RuntimeError, "No factory found.");
+  return false;
+}
+
+std::unique_ptr<ITextureLoader> TextureLoaderFactory::tryCreateInternal(
+    DataReader reader,
+    igl::Result* IGL_NULLABLE outResult) const noexcept {
+  for (const auto& factory : factories_) {
+    auto loader = factory->tryCreate(reader, nullptr);
+    if (loader) {
+      return loader;
+    }
+  }
+  igl::Result::setResult(outResult, igl::Result::Code::RuntimeError, "No factory found.");
+  return nullptr;
+}
+
+} // namespace iglu::textureloader
---
+++ IGLU/texture_loader/TextureLoaderFactory.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <IGLU/texture_loader/ITextureLoaderFactory.h>
+#include <vector>
+
+namespace iglu::textureloader {
+
+/// Factory for creating ITextureLoader instances for supported formats.
+class TextureLoaderFactory : public ITextureLoaderFactory {
+  using Super = ITextureLoaderFactory;
+
+ public:
+  explicit TextureLoaderFactory(std::vector<std::unique_ptr<ITextureLoaderFactory>>&& factories);
+  ~TextureLoaderFactory() override = default;
+
+  [[nodiscard]] uint32_t headerLength() const noexcept final;
+
+ protected:
+  [[nodiscard]] bool canCreateInternal(DataReader headerReader,
+                                       igl::Result* IGL_NULLABLE outResult) const noexcept final;
+
+  [[nodiscard]] std::unique_ptr<ITextureLoader> tryCreateInternal(
+      DataReader reader,
+      igl::Result* IGL_NULLABLE outResult) const noexcept final;
+
+ private:
+  std::vector<std::unique_ptr<ITextureLoaderFactory>> factories_;
+  uint32_t headerLength_;
+};
+
+} // namespace iglu::textureloader
