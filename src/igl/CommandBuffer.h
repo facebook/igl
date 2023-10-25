520cb338651dd2db820ac4d3a6fb9cef03b8d61b
Francois Coulombe <frankyc@meta.com>
2023-10-25 08:01:36 -0700

    Create debug marker guards to avoid forgetting to pop
    Summary: ^
    
    Reviewed By: EricGriffith
    
    Differential Revision: D50613042
    
    fbshipit-source-id: c435b33de9221e965ba65be40129402b47c2a612

--- src/igl/CommandBuffer.h
+++ src/igl/CommandBuffer.h
@@ -150,7 +150,12 @@ class ICommandBuffer {
 #if IGL_DEBUG && !defined(IGL_DISABLE_DEBUG_BUFFER_LABEL)
 #define IGL_DEBUG_BUFFER_LABEL_START(buffer, x) (buffer).pushDebugGroupLabel(x)
 #define IGL_DEBUG_BUFFER_LABEL_END(buffer) (buffer).popDebugGroupLabel()
+#define IGL_DEBUG_BUFFER_LABEL_START_GUARD(buffer, x) \
+  IGL_DEBUG_BUFFER_LABEL_START(buffer, x);            \
+  auto popDebugGroupLabelScope =                      \
+      folly::makeGuard([&cmdBuffer = (buffer)]() { IGL_DEBUG_BUFFER_LABEL_END(cmdBuffer); });
 #else
 #define IGL_DEBUG_BUFFER_LABEL_START(buffer, x)
 #define IGL_DEBUG_BUFFER_LABEL_END(buffer)
+#define IGL_DEBUG_BUFFER_LABEL_START_GUARD(buffer, x)
 #endif // IGL_DEBUG && !defined(IGL_DISABLE_DEBUG_BUFFER_LABEL)
