5b4ca21e401c6f581f52a33e8add616badaaea82
Mauricio Maurer <mmaurer@meta.com>
2023-11-21 11:57:28 -0800

    Document FPSCounter.h
    Summary: Document FPSCounter.h
    
    Reviewed By: KyleFung
    
    Differential Revision: D51497650
    
    fbshipit-source-id: b36786d9b90d541d7b7338b131ca87b2f48f2c30

--- src/igl/FPSCounter.h
+++ src/igl/FPSCounter.h
@@ -11,10 +11,15 @@
 
 namespace igl {
 
-// based on
-// https://github.com/PacktPublishing/3D-Graphics-Rendering-Cookbook/blob/master/shared/UtilsFPS.h
+/// @brief Based on
+/// https://github.com/PacktPublishing/3D-Graphics-Rendering-Cookbook/blob/master/shared/UtilsFPS.h.
+/// Convenience class to measure and print frames per second (FPS) to the console.
 class FPSCounter {
  public:
+  /// @brief Constructs an instance of the class
+  /// @param printToConsole whether to print the FPS count to the console
+  /// @param avgIntervalInSeconds how often to update the FPS counter and print the information to
+  /// the console. The default is 1 second
   explicit FPSCounter(bool printToConsole = true, float avgIntervalInSeconds = 1.0f) :
     printToConsole_(printToConsole), avgIntervalInSeconds_(avgIntervalInSeconds) {
     IGL_ASSERT(avgIntervalInSeconds > 0);
@@ -24,6 +29,12 @@ class FPSCounter {
     return avgFPS_;
   }
 
+  /// @brief Updates the frame count and computes the FPS  if the time passed in as a parameter is
+  /// greater than the threshold set at construciton time. This function should be called when a
+  /// frame has finished being submited for processing by the GPU along with the delta time between
+  /// the current and the previous frame
+  /// @param seconds the time, in seconds, to record and submit this frame's commands. The delta
+  /// time between this frame and the previous frame.
   void updateFPS(double seconds) {
     frames_++;
     time_ += seconds;
