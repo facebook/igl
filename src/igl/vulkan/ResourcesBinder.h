744f099bfec66f2310ed024e9a04563ad1508059
Mauricio Maurer <mmaurer@meta.com>
2023-11-20 16:04:52 -0800

    Document ResourcesBinder.h
    Summary: Document ResourcesBinder.h
    
    Reviewed By: KyleFung
    
    Differential Revision: D51467754
    
    fbshipit-source-id: 08b4026d73c1a8a4ce3f6ee36f8b74f61dec7fcb

--- src/igl/vulkan/ResourcesBinder.h
+++ src/igl/vulkan/ResourcesBinder.h
@@ -32,18 +32,42 @@ struct BindingsTextures {
   igl::vulkan::VulkanSampler* samplers[IGL_TEXTURE_SAMPLERS_MAX] = {};
 };
 
+/** @brief Stores uniform and storage buffer bindings, as well as bindings for textures and sampler
+ * states for Vulkan. This class maintains vectors for each type of shader resource available in IGL
+ * and records the association between binding locations (indices) and the Vulkan objects, while
+ * performing specific checks for each type of resource when they are bound. The associations
+ * between indices and resources is kept locally and does not affect the GPU until
+ * `updateBindings()` is called. This class also records which resource types need to be updated
+ * when `updateBindings()` is called and provides a convenience function to update the descriptor
+ * sets on the context for all resource types. It only performs the update for a resource type that
+ * has been modified after the last call to update the bindings. An instance of this class is bound
+ * to one bind point only (VkPipelineBindPoint), which is VK_PIPELINE_BIND_POINT_GRAPHICS by
+ * default.
+ */
 class ResourcesBinder final {
  public:
   ResourcesBinder(const std::shared_ptr<CommandBuffer>& commandBuffer,
                   const VulkanContext& ctx,
                   VkPipelineBindPoint bindPoint);
 
+  /// @brief Binds a uniform buffer with an offset to index equal to `index`
   void bindUniformBuffer(uint32_t index, igl::vulkan::Buffer* buffer, size_t bufferOffset);
+
+  /// @brief Binds a storage buffer with an offset to index equal to `index`
   void bindStorageBuffer(uint32_t index, igl::vulkan::Buffer* buffer, size_t bufferOffset);
+
+  /// @brief Binds a sampler state to index equal to `index`
   void bindSamplerState(uint32_t index, igl::vulkan::SamplerState* samplerState);
+
+  /// @brief Binds a texture to index equal to `index`
   void bindTexture(uint32_t index, igl::vulkan::Texture* tex);
 
+  /// @brief Convenience function that updates all bindings in the context for all resource types
+  /// that have been modified since the last time this function was called
   void updateBindings();
+
+  /// @brief If the pipeline passed in as a parameter is different than the last pipeline bound
+  /// through this class, binds it and cache it as the last pipeline bound. Does nothing otherwise
   void bindPipeline(VkPipeline pipeline);
 
  private:
