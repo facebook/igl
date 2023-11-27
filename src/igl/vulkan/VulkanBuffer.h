7cde564b323eac0b1a4fe53e6b20372ff8b7ccb7
Mauricio Maurer <mmaurer@meta.com>
2023-11-27 09:54:28 -0800

    Document VulkanBuffer.h
    Summary: Document VulkanBuffer.h
    
    Reviewed By: KyleFung
    
    Differential Revision: D51588429
    
    fbshipit-source-id: c6e80b4d70ba24fd16e8f8e47ba8c6d1b4ba9351

--- src/igl/vulkan/VulkanBuffer.h
+++ src/igl/vulkan/VulkanBuffer.h
@@ -17,8 +17,15 @@ namespace vulkan {
 
 class VulkanContext;
 
+/// @brief A wrapper around a Vulkan Buffer object that provides convenience functions for
+/// uploading/downloading data to/from the GPU.
 class VulkanBuffer {
  public:
+  /** @brief Creates a new VulkanBuffer with a given size, usage flags, memory property flags, and
+   * an optional debug name. Uses VMA if IGL is built with VMA support. If memory flags specify
+   * that the buffer is visible by the host (the CPU), then the buffer's memory will be mapped into
+   * the application's address space and can be accessed directly.
+   */
   VulkanBuffer(const VulkanContext& ctx,
                VkDevice device,
                VkDeviceSize bufferSize,
@@ -30,16 +37,34 @@ class VulkanBuffer {
   VulkanBuffer(const VulkanBuffer&) = delete;
   VulkanBuffer& operator=(const VulkanBuffer&) = delete;
 
+  /** @brief Uploads the data located at `data` into the buffer on the device with the provided
+   * `offset`. Only mapped host-visible buffers can be uploaded this way. All other GPU buffers
+   * should use a temporary staging buffer. If the buffer's data has not been mapped, this function
+   * is a no-op. This function is synchronous and the data is expected to be available when the
+   * function returns
+   */
   void bufferSubData(size_t offset, size_t size, const void* data);
+
+  /** @brief Downloads the data located at `offset` from the buffer on the device to the location
+   * pointed by `data`. Only mapped host-visible buffers can be downloaded this way. All other GPU
+   * buffers should use a temporary staging buffer. If the buffer's data has not been mapped, this
+   * function is a no-op. This function is synchronous and the data is expected to be available
+   * when the function returns.
+   */
   void getBufferSubData(size_t offset, size_t size, void* data);
   [[nodiscard]] uint8_t* getMappedPtr() const {
     return static_cast<uint8_t*>(mappedPtr_);
   }
+
+  /// @brief Whether the buffer's memory has been mapped.
   bool isMapped() const {
     return mappedPtr_ != nullptr;
   }
 
+  /// @brief Flushes the mapped memory range to make it visible to the GPU.
   void flushMappedMemory(VkDeviceSize offset, VkDeviceSize size) const;
+
+  /// @brief Invalidates the mapped memory range to make it visible to the CPU.
   void invalidateMappedMemory(VkDeviceSize offset, VkDeviceSize size) const;
 
   VkBuffer getVkBuffer() const {
