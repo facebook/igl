03868baf76382e8855f8241f8d6aa590d53a5928
Mauricio Maurer <mmaurer@meta.com>
2023-11-28 09:59:10 -0800

    Document ComputeCommandEncoder.h
    Summary: Document `ComputeCommandEncoder.h`
    
    Reviewed By: EricGriffith
    
    Differential Revision: D51617456
    
    fbshipit-source-id: cc6aa783c0c7c92f1a5034302244f1741ac354ce

--- src/igl/vulkan/ComputeCommandEncoder.h
+++ src/igl/vulkan/ComputeCommandEncoder.h
@@ -23,6 +23,7 @@ namespace vulkan {
 
 class VulkanImage;
 
+/// @brief Implements the igl::IComputeCommandEncoder interface for Vulkan
 class ComputeCommandEncoder : public IComputeCommandEncoder {
  public:
   ComputeCommandEncoder(const std::shared_ptr<CommandBuffer>& commandBuffer, VulkanContext& ctx);
@@ -35,17 +36,34 @@ class ComputeCommandEncoder : public IComputeCommandEncoder {
       const std::shared_ptr<IComputePipelineState>& pipelineState) override;
   void dispatchThreadGroups(const Dimensions& threadgroupCount,
                             const Dimensions& threadgroupSize) override;
+
+  /// @brief Ends encoding for compute commands and transitions all images bound to this encoder
+  /// back to `VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL`
   void endEncoding() override;
 
   void pushDebugGroupLabel(const char* label, const igl::Color& color) const override;
   void insertDebugEventLabel(const char* label, const igl::Color& color) const override;
   void popDebugGroupLabel() const override;
+
+  /// @brief This is only for backends that MUST use single uniforms in some situations. Do not
+  /// implement!
   void bindUniform(const UniformDesc& uniformDesc, const void* data) override;
+
+  /// @brief Binds a texture and transitions it to `VK_IMAGE_LAYOUT_GENERAL`. If the texture is not
+  /// a storage texture, this function is a no-op
   void bindTexture(size_t index, ITexture* texture) override;
+
+  /// @brief Binds a buffer. If the buffer is not a storage buffer, this function is a no-op
   void bindBuffer(size_t index, const std::shared_ptr<IBuffer>& buffer, size_t offset) override;
+
+  /// @brief Not implemented
   void bindBytes(size_t index, const void* data, size_t length) override;
+
+  /// @brief Binds push constants pointed by `data` with `length` bytes starting at `offset`.
+  /// `length` must be a multiple of 4.
   void bindPushConstants(const void* data, size_t length, size_t offset) override;
 
+  /// @brief Returns the underlying Vulkan command buffer handle
   VkCommandBuffer getVkCommandBuffer() const {
     return cmdBuffer_;
   }
