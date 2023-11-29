5378bd97278f3d739c523ec4a13faa45018e2626
Mauricio Maurer <mmaurer@meta.com>
2023-11-29 11:26:55 -0800

    Document RenderCommandEncoder.h
    Summary: Document `RenderCommandEncoder.h`
    
    Reviewed By: KyleFung
    
    Differential Revision: D51668067
    
    fbshipit-source-id: fec0125f245ca7405f60cc6fd5545bfc9e3797f0

--- src/igl/vulkan/RenderCommandEncoder.h
+++ src/igl/vulkan/RenderCommandEncoder.h
@@ -20,6 +20,7 @@
 namespace igl {
 namespace vulkan {
 
+/// @brief This class implements the igl::IRenderCommandEncoder interface for Vulkan
 class RenderCommandEncoder : public IRenderCommandEncoder {
  public:
   static std::unique_ptr<RenderCommandEncoder> create(
@@ -35,12 +36,17 @@ class RenderCommandEncoder : public IRenderCommandEncoder {
     endEncoding();
   }
 
+  /// @brief Ends encoding for render commands and transitions the layouts of all images bound to
+  /// this encoder back to `VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL`. Also transitions all
+  /// dependent textures to `VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL`
   void endEncoding() override;
 
   void pushDebugGroupLabel(const char* label, const igl::Color& color) const override;
   void insertDebugEventLabel(const char* label, const igl::Color& color) const override;
   void popDebugGroupLabel() const override;
 
+  /// @brief Sets the viewport size specified in `viewport`. This function flips the viewport in the
+  /// y-direction but retains the same winding as in OpenGL
   void bindViewport(const Viewport& viewport) override;
   void bindScissorRect(const ScissorRect& rect) override;
 
@@ -51,11 +57,19 @@ class RenderCommandEncoder : public IRenderCommandEncoder {
                   uint8_t target,
                   const std::shared_ptr<IBuffer>& buffer,
                   size_t bufferOffset) override;
+
+  /// @brief Not implemented
   void bindBytes(size_t index, uint8_t target, const void* data, size_t length) override;
+
+  /// @brief Binds push constants pointed by `data` with `length` bytes starting at `offset`.
+  /// `length` must be a multiple of 4.
   void bindPushConstants(const void* data, size_t length, size_t offset) override;
   void bindSamplerState(size_t index, uint8_t target, ISamplerState* samplerState) override;
 
   void bindTexture(size_t index, uint8_t target, ITexture* texture) override;
+
+  /// @brief This is only for backends that MUST use single uniforms in some situations. Do not
+  /// implement!
   void bindUniform(const UniformDesc& uniformDesc, const void* data) override;
 
   void draw(PrimitiveType primitiveType, size_t vertexStart, size_t vertexCount) override;
@@ -95,11 +109,20 @@ class RenderCommandEncoder : public IRenderCommandEncoder {
     return binder_;
   }
 
+  /// @brief Enables or disables the draw call count. If enabled, it will increment the draw call,
+  /// otherwise it won't. This is used to disable the draw call count when we are doing auxiliary
+  /// draw calls such as shader debugging.
   bool setDrawCallCountEnabled(bool value);
 
  private:
   void bindPipeline();
+
+  /// @brief Ensures that the vertex buffers are bound by performing checks. If the function doesn't
+  /// assert at some point, the vertex buffer(s) is bound correctly.
   void ensureVertexBuffers();
+
+  /// @brief Ensures that all shader bindings are bound by checking the SPIR-V reflection. If the
+  /// function doesn't assert at some point, the shader bindings are correct.
   void ensureShaderModule(IShaderModule* sm);
 
  private:
