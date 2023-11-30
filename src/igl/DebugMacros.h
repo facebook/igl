c8222d05aa1ae07e831034e3144ff71164fead92
Francois Coulombe <frankyc@meta.com>
2023-11-30 10:35:39 -0800

    Moving Renderdoc markers to their own file so that force switching them on doesn't trigger such a massive rebuild
    Summary:
    Modifying CommandBuffer.h triggers massive rebuild.
    Moving this to its own header in hope of reducing the reach
    
    Reviewed By: EricGriffith
    
    Differential Revision: D51662435
    
    fbshipit-source-id: 333063a0393e7e2415832a6c3383cede523e10ae

---
+++ src/igl/DebugMacros.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <igl/CommandBuffer.h>
+
+// Debug macros that take in an igl CommandBuffer and set some debug labels to group the drawcalls
+#if IGL_DEBUG && !defined(IGL_DISABLE_DEBUG_BUFFER_LABEL)
+#define IGL_DEBUG_BUFFER_LABEL_START(buffer, x) (buffer)->pushDebugGroupLabel(x);
+#define IGL_DEBUG_BUFFER_LABEL_END(buffer) (buffer)->popDebugGroupLabel()
+#define IGL_DEBUG_BUFFER_LABEL_START_GUARD(buffer, x) \
+  IGL_DEBUG_BUFFER_LABEL_START(buffer, x);            \
+  auto popDebugGroupLabelScope =                      \
+      folly::makeGuard([cmdBuffer = (buffer)]() { IGL_DEBUG_BUFFER_LABEL_END(cmdBuffer); });
+#else
+#define IGL_DEBUG_BUFFER_LABEL_START(buffer, x)
+#define IGL_DEBUG_BUFFER_LABEL_END(buffer)
+#define IGL_DEBUG_BUFFER_LABEL_START_GUARD(buffer, x)
+#endif // IGL_DEBUG && !defined(IGL_DISABLE_DEBUG_BUFFER_LABEL)
