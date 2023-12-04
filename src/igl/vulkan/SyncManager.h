5bcd29ef8556ec381e8b49c42133e2bc77943bd8
Mauricio Maurer <mmaurer@meta.com>
2023-12-04 12:34:29 -0800

    Document SyncManager.h
    Summary: Document SyncManager.h
    
    Reviewed By: KyleFung
    
    Differential Revision: D51812970
    
    fbshipit-source-id: a5b98014a51225b8efb9e3d02edd130a3b0f431b

--- src/igl/vulkan/SyncManager.h
+++ src/igl/vulkan/SyncManager.h
@@ -17,18 +17,29 @@ namespace vulkan {
 
 class VulkanContext;
 
+/// @brief A manager that keeps track of ring buffer properties for buffers, such as the number of
+/// sub-buffers to allocate and the current index of the sub-buffer being used.
 class SyncManager final {
  public:
   using SubmitHandle = VulkanImmediateCommands::SubmitHandle;
 
+  /// @brief Constructs a SyncManager object with the maximum number of resources to allocate. This
+  /// class doesn't allocate the resources. It merely keeps track of the current index and the
+  /// maximum number of resources that exist in the system.
   explicit SyncManager(const VulkanContext& ctx, uint32_t maxResourceCount);
 
+  /// @brief Returns the index of the current resource being used. Its range is [0,
+  /// maxResourceCount).
   [[nodiscard]] uint32_t currentIndex() const noexcept;
 
+  /// @brief Returns the maximum number of resources that must be allocated by ring buffers.
   [[nodiscard]] uint32_t maxResourceCount() const noexcept;
 
+  /// @brief Increments the current index and waits for newly computed index's SubmitHandle to
+  /// become free before continuing.
   void acquireNext() noexcept;
 
+  /// @brief Marks the given handle as submitted.
   void markSubmitted(SubmitHandle handle) noexcept;
 
  private:
