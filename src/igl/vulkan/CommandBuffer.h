ba6b9ede724cdbf3a2ba59176f4a15889c187d80
Mauricio Maurer <mmaurer@meta.com>
2023-12-05 18:00:57 -0800

    Document CommandBuffer.h
    Summary: Document CommandBuffer.h
    
    Reviewed By: KyleFung
    
    Differential Revision: D51812937
    
    fbshipit-source-id: cf1c76ac483f3ffb38adcfe1a8c605386df036d4

--- src/igl/vulkan/CommandBuffer.h
+++ src/igl/vulkan/CommandBuffer.h
@@ -17,27 +17,53 @@ namespace vulkan {
 class Buffer;
 class VulkanContext;
 
+/// @brief This class implements the igl::ICommandBuffer interface for Vulkan
 class CommandBuffer final : public ICommandBuffer,
                             public std::enable_shared_from_this<CommandBuffer> {
  public:
+  /// @brief Constructs a CommandBuffer object, acquires a
+  /// `VulkanImmediateCommands::CommandBufferWrapper` from the context's VulkanImmediateCommands
+  /// object, and stores the CommandBufferDesc structure used to construct the underlying command
+  /// buffer.
   CommandBuffer(VulkanContext& ctx, CommandBufferDesc desc);
 
+  /// @brief Creates a ComputeCommandEncoder
   std::unique_ptr<IComputeCommandEncoder> createComputeCommandEncoder() override;
 
+  /** @brief Creates a RenderCommandEncoder
+   * Before creating a RenderCommandEncoder, this function transitions all images referenced by the
+   * `dependencies` parameter to a shader read only layout. It also transitions all images
+   * referenced by the `framebuffer` parameter to their optimal layout: color and resolve
+   * attachments are transitioned to color attachment optimal layouts; the depth/stencil is
+   * transitioned to depth/stencil attachment optimal layout. Once the RenderCommandEncoder has been
+   * created, and if there is an enhanced shader debugging store object defined in the contest, this
+   * function also binds an extra storage buffer used by the shader debugging functionality. Returns
+   * a RenderCommandEncoder object.
+   */
   std::unique_ptr<IRenderCommandEncoder> createRenderCommandEncoder(
       const RenderPassDesc& renderPass,
       std::shared_ptr<IFramebuffer> framebuffer,
       const Dependencies& dependencies,
       Result* outResult) override;
 
+  /** @brief Caches the texture passed in to the function for presentation later. Due to the
+   * enhanced shader debugging functionality, the image cannot be presented here. It can only be
+   * presented after the command buffer has been submitted, processed and then used by the enhanced
+   * shader debugging functionality. If the texture belongs to a swapchain, this function
+   * transitions the texture to VK_IMAGE_LAYOUT_PRESENT_SRC_KHR layout. Otherwise it transitions the
+   * texture to the VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL layout if the texture's samples is
+   * equal to 1 (it's non multi-sampled).
+   */
   void present(std::shared_ptr<ITexture> surface) const override;
 
   void pushDebugGroupLabel(const char* label, const igl::Color& color) const override;
 
   void popDebugGroupLabel() const override;
 
+  /// @brief Waits until the command bufer has been executed by the device.
   void waitUntilCompleted() override;
 
+  /// @brief Not implemented
   void waitUntilScheduled() override;
 
   VkCommandBuffer getVkCommandBuffer() const {
