98618a05c38b420b2f076db80bbd10fd7af3a868
Sergey Kosarevsky <sergkosarevsky@meta.com>
2024-03-05 11:03:28 -0800

    igl | shell | Add GPUStressSession to CMake builds
    Summary: Added `GPUStressSession` to CMake builds.
    
    Reviewed By: EricGriffith
    
    Differential Revision: D54499856
    
    fbshipit-source-id: 994bfae83d0da9f1cbbc712ff9ffe0183df0230a

--- shell/openxr/desktop/main.cpp
+++ shell/openxr/desktop/main.cpp
@@ -11,7 +11,7 @@
 
 using namespace igl::shell::openxr;
 
-#if USE_VULKAN_BACKEND
+#if USE_VULKAN_BACKEND || IGL_BACKEND_VULKAN
 #include "vulkan/XrAppImplVulkan.h"
 // @fb-only
 // @fb-only
@@ -25,7 +25,7 @@ XrInstance getXrInstance() {
 }
 
 int main(int argc, const char* argv[]) {
-#if USE_VULKAN_BACKEND
+#if USE_VULKAN_BACKEND || IGL_BACKEND_VULKAN
   auto xrApp = std::make_unique<XrApp>(std::make_unique<desktop::XrAppImplVulkan>());
 // @fb-only
   // @fb-only
