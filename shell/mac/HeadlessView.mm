f926c667889845736360c699de91f862f57e7cde
Michael Tay <michaeltay@meta.com>
2024-03-12 09:14:38 -0700

    Update Headless View Controller
    Summary:
    Headless view controller was not setup correctly.
    This diff fixes it so that display link is hook up for render dispatch to be called correctly.
    
    Reviewed By: gnolnuhc
    
    Differential Revision: D54705841
    
    fbshipit-source-id: db698ede1f779d3f13fd544c8d3296da86c96d15

---
+++ shell/mac/HeadlessView.mm
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#import "HeadlessView.h"
+
+#import "ViewController.h"
+
+#import <Foundation/Foundation.h>
+
+@interface HeadlessView () {
+  CVDisplayLinkRef displayLink_; // display link for managing rendering thread
+  NSTrackingArea* trackingArea_; // needed for mouseMoved: events
+}
+@property (weak) ViewController* viewController;
+@end
+
+@implementation HeadlessView
+
+- (void)dealloc {
+  CVDisplayLinkRelease(displayLink_);
+}
+
+- (void)prepareHeadless {
+  NSApplication* app = [NSApplication sharedApplication];
+  NSWindow* window = [app windows][0];
+  NSTabViewController* tabController = (NSTabViewController*)window.contentViewController;
+  NSTabViewItem* item = tabController.tabViewItems[tabController.selectedTabViewItemIndex];
+
+  ViewController* controller = (ViewController*)item.viewController;
+  self.viewController = controller;
+  [controller initModule];
+
+  [self initTimer];
+  [self startTimer];
+}
+
+static CVReturn DisplayLinkCallback(CVDisplayLinkRef /*displayLink*/,
+                                    const CVTimeStamp* /*now*/,
+                                    const CVTimeStamp* /*outputTime*/,
+                                    CVOptionFlags /*flagsIn*/,
+                                    CVOptionFlags* /*flagsOut*/,
+
+                                    void* userdata) {
+  auto view = (__bridge HeadlessView*)userdata;
+  [view.viewController performSelectorOnMainThread:@selector(render)
+                                        withObject:nil
+                                     waitUntilDone:YES];
+  return kCVReturnSuccess;
+}
+
+- (void)initTimer {
+  // Create a display link capable of being used with all active displays
+  CVDisplayLinkCreateWithActiveCGDisplays(&displayLink_);
+
+  // Set the renderer output callback function
+  CVDisplayLinkSetOutputCallback(displayLink_, &DisplayLinkCallback, (__bridge void*)self);
+}
+
+- (void)startTimer {
+  CVDisplayLinkStart(displayLink_);
+}
+
+- (void)stopTimer {
+  CVDisplayLinkStop(displayLink_);
+}
+
+- (void)addFullScreenTrackingArea {
+  trackingArea_ =
+      [[NSTrackingArea alloc] initWithRect:self.bounds
+                                   options:NSTrackingMouseEnteredAndExited | NSTrackingMouseMoved |
+                                           NSTrackingActiveInKeyWindow
+                                     owner:self
+                                  userInfo:nil];
+  [self addTrackingArea:trackingArea_];
+}
+
+- (void)updateTrackingAreas {
+  [self removeTrackingArea:trackingArea_];
+  [self addFullScreenTrackingArea];
+}
+
+@end
