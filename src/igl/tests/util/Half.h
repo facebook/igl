080f96afa0c423ddd13a51c1f3d0adbe0e4c2db8
Francois Coulombe <frankyc@meta.com>
2024-03-13 09:15:04 -0700

    Allow our test half to use half lib or glm implementation
    Summary:
    There are some issues with the glm implementation with template specialization and I'm not enough of a template buff to fix them.
    The open source library half doesn't suffer from this so instead of re-inventing the wheel, I'm using what has already been done and optimized.
    
    Reviewed By: EricGriffith
    
    Differential Revision: D54683923
    
    fbshipit-source-id: fdf0d19eaf257bf1e279c0ed7f368807c68e7b0c

--- src/igl/tests/util/Half.h
+++ src/igl/tests/util/Half.h
@@ -7,26 +7,46 @@
 
 #pragma once
 
+#define IGL_USE_CUSTOM_HALF 1
+
+#if IGL_USE_CUSTOM_HALF
 #include <glm/gtc/packing.hpp>
 
 namespace igl::tests::util {
-
 struct Half {
  public:
-  Half() {}
+  constexpr Half() noexcept {}
+
   explicit Half(float f) {
     data_ = glm::packHalf1x16(f);
   }
-  Half operator=(float f) {
+  Half& operator=(float f) {
     data_ = glm::packHalf1x16(f);
     return *this;
   }
+
   operator float() const {
     return glm::unpackHalf1x16(data_);
   }
 
  private:
   uint16_t data_ = 0;
+
+  template<typename charT, typename traits>
+  friend std::basic_ostream<charT, traits>& operator<<(std::basic_ostream<charT, traits>&, Half);
 };
 
+template<typename charT, typename traits>
+std::basic_ostream<charT, traits>& operator<<(std::basic_ostream<charT, traits>& out, Half arg) {
+  return out << glm::unpackHalf1x16(arg.data_);
+}
+
+using TestHalf = Half;
 } // namespace igl::tests::util
+#else
+#include <half/half.hpp>
+
+namespace igl::tests::util {
+using TestHalf = half_float::half;
+} // namespace igl::tests::util
+#endif
