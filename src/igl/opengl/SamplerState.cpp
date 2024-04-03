46267d4546a38372fdad17ff4ae102ae423934de
Francois Coulombe <frankyc@meta.com>
2024-04-03 13:20:20 -0700

    Disable the anisotropic filter for now, it's causing a crash on some devices
    Summary:
    we need to add an "enabled" flag like on vulkan and metal to avoid performing the call if this has not been set by the user.
    
    we probably also need to investigate why it's crashing on external textures on those phones. it might not be supported.
    
    Reviewed By: EricGriffith, marekcirkos
    
    Differential Revision: D55692051
    
    fbshipit-source-id: 1639b397d880743f697139eef9d8bd2aab0ecf81

--- src/igl/opengl/SamplerState.cpp
+++ src/igl/opengl/SamplerState.cpp
@@ -95,7 +95,13 @@ void SamplerState::bind(ITexture* t) {
     getContext().texParameteri(target, GL_TEXTURE_MAX_LOD, mipLodMax_);
   }
   if (deviceFeatures.hasFeature(DeviceFeatures::TextureFilterAnisotropic)) {
+    // @fb-only
+    // Disable the anisotropic filter for now, it's causing a crash on some devices
+#if 0
     getContext().texParameteri(target, GL_TEXTURE_MAX_ANISOTROPY, maxAnisotropy_);
+#else
+    (void)maxAnisotropy_;
+#endif
   }
 
   if (isDepthOrDepthStencil &&
