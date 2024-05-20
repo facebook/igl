d6970a336886156e2b2d85c632bc8ef344c833eb
Sergey Kosarevsky <sergkosarevsky@meta.com>
2024-05-20 13:37:33 -0700

    IGLU | Migrate `simple_renderer` to use the new `drawIndexed()` API
    Summary: Migrate `simple_renderer` to use the new `drawIndexed()` API.
    
    Reviewed By: trivedivivek, mmaurer
    
    Differential Revision: D57421216
    
    fbshipit-source-id: 842eeafb104ec7d13250cf40fd90d03959ae3698

--- IGLU/simple_renderer/VertexData.cpp
+++ IGLU/simple_renderer/VertexData.cpp
@@ -76,8 +76,8 @@ void VertexData::draw(igl::IRenderCommandEncoder& commandEncoder) {
   }
 
   if (ib_) {
-    commandEncoder.drawIndexed(
-        primitiveDesc_.type, primitiveDesc_.numEntries, ibFormat_, *ib_, primitiveDesc_.offset);
+    commandEncoder.bindIndexBuffer(*ib_, ibFormat_, primitiveDesc_.offset);
+    commandEncoder.drawIndexed(primitiveDesc_.type, primitiveDesc_.numEntries);
   } else {
     commandEncoder.draw(primitiveDesc_.type, primitiveDesc_.offset, primitiveDesc_.numEntries);
   }
