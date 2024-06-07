824a29489b0f901ca6addc6b2b892c882f412172
Mauricio Maurer <mmaurer@meta.com>
2024-06-07 11:29:11 -0700

    Add test for ivkGetAttachmentDescription
    Summary: Title
    
    Reviewed By: corporateshark
    
    Differential Revision: D58289468
    
    fbshipit-source-id: e51d3f8e70b628ab099884d1812adc64be0cf5c5

--- src/igl/tests/vulkan/VulkanHelpersTest.cpp
+++ src/igl/tests/vulkan/VulkanHelpersTest.cpp
@@ -20,7 +20,7 @@ namespace igl::tests {
 // Unit tests for functions in VulkanHelpers.{h|c}.
 //
 
-// GetDescriptorSetLayoutBinding **************************************************************
+// ivkGetDescriptorSetLayoutBinding **************************************************************
 
 class DescriptorSetLayoutTest
   : public ::testing::TestWithParam<std::tuple<uint32_t, VkDescriptorType, uint32_t>> {};
@@ -55,6 +55,57 @@ INSTANTIATE_TEST_SUITE_P(
       const std::string name = "binding_" + std::to_string(std::get<0>(info.param)) +
                                "__descriptorType_" + std::to_string(std::get<1>(info.param)) +
                                "__count_" + std::to_string(std::get<2>(info.param));
+      return name;
+    });
+
+// ivkGetAttachmentDescription **************************************************************
+
+class AttachmentDescriptionTest
+  : public ::testing::TestWithParam<std::tuple<VkFormat,
+                                               VkAttachmentLoadOp,
+                                               VkAttachmentStoreOp,
+                                               VkImageLayout,
+                                               VkImageLayout,
+                                               VkSampleCountFlagBits>> {};
+
+TEST_P(AttachmentDescriptionTest, GetAttachmentDescription) {
+  const VkFormat format = std::get<0>(GetParam());
+  const VkAttachmentLoadOp loadOp = std::get<1>(GetParam());
+  const VkAttachmentStoreOp storeOp = std::get<2>(GetParam());
+  const VkImageLayout initialLayout = std::get<3>(GetParam());
+  const VkImageLayout finalLayout = std::get<4>(GetParam());
+  const VkSampleCountFlagBits samples = std::get<5>(GetParam());
+
+  const auto attachmentDescription =
+      ivkGetAttachmentDescription(format, loadOp, storeOp, initialLayout, finalLayout, samples);
+  EXPECT_EQ(attachmentDescription.flags, 0);
+  EXPECT_EQ(attachmentDescription.format, format);
+  EXPECT_EQ(attachmentDescription.samples, samples);
+  EXPECT_EQ(attachmentDescription.loadOp, loadOp);
+  EXPECT_EQ(attachmentDescription.storeOp, storeOp);
+  EXPECT_EQ(attachmentDescription.stencilLoadOp, VK_ATTACHMENT_LOAD_OP_DONT_CARE);
+  EXPECT_EQ(attachmentDescription.stencilStoreOp, VK_ATTACHMENT_STORE_OP_DONT_CARE);
+  EXPECT_EQ(attachmentDescription.initialLayout, initialLayout);
+  EXPECT_EQ(attachmentDescription.finalLayout, finalLayout);
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    AllCombinations,
+    AttachmentDescriptionTest,
+    ::testing::Combine(
+        ::testing::Values(VK_FORMAT_R8G8B8_UNORM, VK_FORMAT_R8G8B8_SRGB),
+        ::testing::Values(VK_ATTACHMENT_LOAD_OP_DONT_CARE, VK_ATTACHMENT_LOAD_OP_LOAD),
+        ::testing::Values(VK_ATTACHMENT_STORE_OP_DONT_CARE, VK_ATTACHMENT_STORE_OP_STORE),
+        ::testing::Values(VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL),
+        ::testing::Values(VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL),
+        ::testing::Values(VK_SAMPLE_COUNT_1_BIT, VK_SAMPLE_COUNT_4_BIT)),
+    [](const testing::TestParamInfo<AttachmentDescriptionTest::ParamType>& info) {
+      const std::string name = "format_" + std::to_string(std::get<0>(info.param)) + "__loadOp_" +
+                               std::to_string(std::get<1>(info.param)) + "__storeOp_" +
+                               std::to_string(std::get<2>(info.param)) + "__initialLayout_" +
+                               std::to_string(std::get<3>(info.param)) + "__finalLayout_" +
+                               std::to_string(std::get<4>(info.param)) + "__samples_" +
+                               std::to_string(std::get<5>(info.param));
       return name;
     });
 
