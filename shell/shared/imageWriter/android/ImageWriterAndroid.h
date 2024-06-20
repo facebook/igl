65390356e26801a7459bb9a2d8f94d3faced81e5
Ruslan Shestopalyuk <rshest@meta.com>
2024-06-19 22:19:12 -0700

    Use ImageWriterSTB on Android
    Summary:
    Writing images in IGL on Android platform was stubbed with "not implemented" stub, which asserted.
    
    It should be just fine using the regular `ImageWriterSTB`, which writes to a file system - arguably, writing into the app's resources doesn't make much practical sense, and we don't have to be symmetrical with the reader, which *does* read from the resources (which the stub appeared to assume).
    
    In RN/Granite, we want this functionality to be available for:
    * running screenshot tests on device
    * caching images on disk, which are created from pixel data
    * debugging (which has been indeed very useful in my case, when debugging OpenGL ES drawing)
    
    Reviewed By: christophpurrer
    
    Differential Revision: D58747911

--- shell/shared/imageWriter/android/ImageWriterAndroid.h
+++ shell/shared/imageWriter/android/ImageWriterAndroid.h
@@ -7,30 +7,10 @@
 
 #pragma once
 
-#include <android/log.h>
-#include <shell/shared/imageWriter/ImageWriter.h>
-#include <string>
-
-struct AAssetManager;
+#include <shell/shared/imageWriter/stb/ImageWriterSTB.h>
 
 namespace igl::shell {
 
-class ImageWriterAndroid final : public ImageWriter {
- public:
-  ImageWriterAndroid() = default;
-  ~ImageWriterAndroid() override = default;
-
-  void writeImage(const std::string& /*imageAbsolutePath*/,
-                  const ImageData& imageData) const noexcept override;
-  void setAssetManager(AAssetManager* mgr) {
-    assetManager_ = mgr;
-  }
-  AAssetManager* getAssetManager() const noexcept {
-    return assetManager_;
-  }
-
- private:
-  AAssetManager* assetManager_;
-};
+using ImageWriterAndroid = ImageWriterSTB;
 
 } // namespace igl::shell
