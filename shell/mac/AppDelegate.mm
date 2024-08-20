e97def57ae3788bacd379010dac9c31b66af0b56
Eric Griffith <ericgriffith@meta.com>
2024-08-20 13:23:17 -0700

    IGL: Set sample app window colorspace to sRGB
    Summary: This diff updates the sample app window's colorspace to sRGB. This lets the ColorSession render the correct colors for Metal, OpenGL and Vulkan.
    
    Differential Revision: D61514829
    
    fbshipit-source-id: 2ded7914cb7f9ec04ce72972301c0f8cece2a544

--- shell/mac/AppDelegate.mm
+++ shell/mac/AppDelegate.mm
@@ -24,6 +24,7 @@
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification*)aNotification {
+  [self.window setColorSpace:[NSColorSpace sRGBColorSpace]];
   [self setupViewController];
   [self.window makeKeyAndOrderFront:nil];
   [self.window makeFirstResponder:self.tabViewController.view];
