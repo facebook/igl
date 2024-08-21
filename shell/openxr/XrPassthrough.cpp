03a95da50115c4186aaf4dd4c2e9d44c7a854ad9
Sergey Kosarevsky <sergkosarevsky@meta.com>
2024-08-21 12:49:07 -0700

    igl | openxr | Fix a validation error when `XrPassthroughCreateInfoFB::flags` are empty
    Summary:
    Fixed a validation error:
    
    https://registry.khronos.org/OpenXR/specs/1.1/man/html/XrPassthroughCreateInfoFB.html
    
    The `XrPassthroughCreateInfoFB` structure is defined as:
    
    ```
    // Provided by XR_FB_passthrough
    typedef struct XrPassthroughCreateInfoFB {
        XrStructureType         type;
        const void*             next;
        XrPassthroughFlagsFB    flags;
    } XrPassthroughCreateInfoFB;
    ```
    
    Valid Usage (Implicit)
    
    * The `XR_FB_passthrough` extension must be enabled prior to using `XrPassthroughCreateInfoFB`.
    
    * `type` must be `XR_TYPE_PASSTHROUGH_CREATE_INFO_FB`.
    
    * `next` must be `NULL` or a valid pointer to the next structure in a structure chain.
    
    * `flags` must be a valid combination of `XrPassthroughFlagBitsFB` values.
    
    * **`flags` must not be `0`.**
    
    Reviewed By: rokuz
    
    Differential Revision: D61568757
    
    fbshipit-source-id: e596b82364c8305b7f0a317b5519eb085a08b6c1

--- shell/openxr/XrPassthrough.cpp
+++ shell/openxr/XrPassthrough.cpp
@@ -52,7 +52,10 @@ const std::vector<const char*>& XrPassthrough::getExtensions() noexcept {
 
 bool XrPassthrough::initialize() noexcept {
   const XrPassthroughCreateInfoFB passthroughInfo{
-      .type = XR_TYPE_PASSTHROUGH_CREATE_INFO_FB, .next = nullptr, .flags = 0u};
+      .type = XR_TYPE_PASSTHROUGH_CREATE_INFO_FB,
+      .next = nullptr,
+      .flags = XR_PASSTHROUGH_IS_RUNNING_AT_CREATION_BIT_FB,
+  };
 
   XrResult result;
   XR_CHECK(result = xrCreatePassthroughFB_(session_, &passthroughInfo, &passthrough_));
