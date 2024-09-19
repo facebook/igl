a5cfe3b433397b12ffec6f6e0b3cedd329b18686
Francois Coulombe <frankyc@meta.com>
2024-09-19 03:18:24 -0700

    Refactor the color space input to the shell app so that it can be controlled by a centralized flag across all apis
    Summary: ^^
    
    Reviewed By: EricGriffith, maliarov
    
    Differential Revision: D62763546
    
    fbshipit-source-id: 51016dea98f4b1d8c3f3a6bf526b46e924baf5e1

---
+++ src/igl/metal/ColorSpace.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+#import <CoreGraphics/CGColorSpace.h>
+#include <igl/ColorSpace.h>
+
+namespace igl::metal {
+CGColorSpaceRef colorSpaceToCGColorSpace(igl::ColorSpace colorSpace);
+} // namespace igl::metal
---
+++ src/igl/metal/ColorSpace.mm
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <igl/metal/ColorSpace.h>
+
+namespace igl::metal {
+CGColorSpaceRef colorSpaceToCGColorSpace(igl::ColorSpace colorSpace) {
+  switch (colorSpace) {
+  case ColorSpace::SRGB_LINEAR:
+    return CGColorSpaceCreateWithName(kCGColorSpaceLinearSRGB);
+  case ColorSpace::SRGB_NONLINEAR:
+    return CGColorSpaceCreateWithName(kCGColorSpaceSRGB);
+  case ColorSpace::DISPLAY_P3_NONLINEAR:
+    return CGColorSpaceCreateWithName(kCGColorSpaceDisplayP3);
+  case ColorSpace::DISPLAY_P3_LINEAR:
+    return CGColorSpaceCreateWithName(kCGColorSpaceLinearDisplayP3);
+  case ColorSpace::EXTENDED_SRGB_LINEAR:
+    return CGColorSpaceCreateWithName(kCGColorSpaceExtendedLinearSRGB);
+  case ColorSpace::DCI_P3_NONLINEAR:
+    return CGColorSpaceCreateWithName(kCGColorSpaceDCIP3);
+  case ColorSpace::BT709_LINEAR:
+    return CGColorSpaceCreateWithName(kCGColorSpaceLinearSRGB);
+  case ColorSpace::BT709_NONLINEAR:
+    return CGColorSpaceCreateWithName(kCGColorSpaceITUR_709);
+  case ColorSpace::BT2020_LINEAR:
+    return CGColorSpaceCreateWithName(kCGColorSpaceLinearITUR_2020);
+  case ColorSpace::ADOBERGB_LINEAR:
+    return CGColorSpaceCreateWithName(kCGColorSpaceAdobeRGB1998);
+  case ColorSpace::ADOBERGB_NONLINEAR:
+    return CGColorSpaceCreateWithName(kCGColorSpaceAdobeRGB1998);
+  case ColorSpace::PASS_THROUGH:
+    return nil;
+  case ColorSpace::EXTENDED_SRGB_NONLINEAR:
+    return CGColorSpaceCreateWithName(kCGColorSpaceExtendedSRGB);
+  case ColorSpace::BT2020_NONLINEAR:
+    return CGColorSpaceCreateWithName(kCGColorSpaceITUR_2020);
+  case ColorSpace::HDR10_ST2084:
+  case ColorSpace::DOLBYVISION:
+  case ColorSpace::HDR10_HLG:
+  case ColorSpace::DISPLAY_NATIVE_AMD:
+  case ColorSpace::BT601_NONLINEAR:
+  case ColorSpace::BT2100_HLG_NONLINEAR:
+  case ColorSpace::BT2100_PQ_NONLINEAR:
+  default:
+    IGL_ASSERT_NOT_IMPLEMENTED();
+    return CGColorSpaceCreateWithName(kCGColorSpaceSRGB);
+  }
+  IGL_UNREACHABLE_RETURN(CGColorSpaceCreateWithName(kCGColorSpaceSRGB));
+}
+} // namespace igl::metal
