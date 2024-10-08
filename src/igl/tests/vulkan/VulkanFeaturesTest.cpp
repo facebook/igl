c11c9ea6cd03e32715862a673f73a8fc32e90732
Mauricio Maurer <mmaurer@meta.com>
2024-10-08 14:01:18 -0700

    igl | vulkan | tests | Add test for VulkanFeatures::enableDefaultFeatures1_1
    Reviewed By: pixelperfect3, corporateshark
    
    Differential Revision: D64055432
    
    fbshipit-source-id: d808e84da25b454e152658fdb42ddf2cafacc88a

--- src/igl/tests/vulkan/VulkanFeaturesTest.cpp
+++ src/igl/tests/vulkan/VulkanFeaturesTest.cpp
@@ -70,4 +70,53 @@ TEST_F(VulkanFeaturesTest, CopyNotPerformed) {
   EXPECT_TRUE(configDst.enableDescriptorIndexing);
 }
 
+// Enable Default Features Vk 1.1 ****************************************************
+TEST_F(VulkanFeaturesTest, EnableDefaultFeatures_Vk_1_1) {
+  const igl::vulkan::VulkanContextConfig config;
+
+  igl::vulkan::VulkanFeatures features(VK_API_VERSION_1_1, config);
+  features.enableDefaultFeatures1_1();
+
+  EXPECT_EQ(features.version_, VK_API_VERSION_1_1);
+  EXPECT_FALSE(features.VkPhysicalDeviceShaderFloat16Int8Features_.shaderFloat16);
+  EXPECT_FALSE(features.VkPhysicalDeviceShaderFloat16Int8Features_.shaderInt8);
+
+  EXPECT_TRUE(features.VkPhysicalDeviceFeatures2_.features.dualSrcBlend);
+  EXPECT_TRUE(features.VkPhysicalDeviceFeatures2_.features.shaderInt16);
+  EXPECT_TRUE(features.VkPhysicalDeviceFeatures2_.features.multiDrawIndirect);
+  EXPECT_TRUE(features.VkPhysicalDeviceFeatures2_.features.drawIndirectFirstInstance);
+  EXPECT_TRUE(features.VkPhysicalDeviceFeatures2_.features.depthBiasClamp);
+#ifdef IGL_PLATFORM_ANDROID
+  // fillModeNonSolid is not well supported on Android, only enable by default when it's not android
+  EXPECT_FALSE(features.VkPhysicalDeviceFeatures2_.features.fillModeNonSolid);
+#else
+  EXPECT_TRUE(features.VkPhysicalDeviceFeatures2_.features.fillModeNonSolid);
+#endif
+
+#if defined(VK_EXT_descriptor_indexing) && VK_EXT_descriptor_indexing
+  if (config.enableDescriptorIndexing) {
+    auto& descriptorIndexingFeatures = features.VkPhysicalDeviceDescriptorIndexingFeaturesEXT_;
+    EXPECT_TRUE(descriptorIndexingFeatures.shaderSampledImageArrayNonUniformIndexing);
+    EXPECT_TRUE(descriptorIndexingFeatures.descriptorBindingUniformBufferUpdateAfterBind);
+    EXPECT_TRUE(descriptorIndexingFeatures.descriptorBindingSampledImageUpdateAfterBind);
+    EXPECT_TRUE(descriptorIndexingFeatures.descriptorBindingStorageImageUpdateAfterBind);
+    EXPECT_TRUE(descriptorIndexingFeatures.descriptorBindingStorageBufferUpdateAfterBind);
+    EXPECT_TRUE(descriptorIndexingFeatures.descriptorBindingUpdateUnusedWhilePending);
+    EXPECT_TRUE(descriptorIndexingFeatures.descriptorBindingPartiallyBound);
+    EXPECT_TRUE(descriptorIndexingFeatures.runtimeDescriptorArray);
+  }
+#endif
+
+  EXPECT_TRUE(features.VkPhysicalDevice16BitStorageFeatures_.storageBuffer16BitAccess);
+
+#if defined(VK_KHR_buffer_device_address) && VK_KHR_buffer_device_address
+  if (config.enableBufferDeviceAddress) {
+    EXPECT_TRUE(features.VkPhysicalDeviceBufferDeviceAddressFeaturesKHR_.bufferDeviceAddress);
+  }
+#endif
+  EXPECT_TRUE(features.VkPhysicalDeviceMultiviewFeatures_.multiview);
+  EXPECT_TRUE(features.VkPhysicalDeviceSamplerYcbcrConversionFeatures_.samplerYcbcrConversion);
+  EXPECT_TRUE(features.VkPhysicalDeviceShaderDrawParametersFeatures_.shaderDrawParameters);
+}
+
 } // namespace igl::tests
