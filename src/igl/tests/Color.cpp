02408ddcadf28189388e7d315d2b11b5161f29f8
Francois Coulombe <frankyc@meta.com>
2024-10-16 16:38:10 -0700

    Create sRGB Color test
    Reviewed By: mmaurer
    
    Differential Revision: D64466742
    
    fbshipit-source-id: 80a1a1722d22dba4699c05d8b4b15f0afb51ad09

--- src/igl/tests/Color.cpp
+++ src/igl/tests/Color.cpp
@@ -5,6 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+#include "util/Color.h"
 #include <gtest/gtest.h>
 #include <igl/Color.h>
 
@@ -29,5 +30,33 @@ TEST(ColorTest, ctor) {
   ASSERT_EQ(floatPtr[1], 0.5f);
   ASSERT_EQ(floatPtr[2], 0.0f);
   ASSERT_EQ(floatPtr[3], 1.0f);
+}
+
+TEST(sRGBColorTest, ctor) {
+  const util::sRGBColor testColor(255, 128, 0);
+  ASSERT_EQ(testColor.r, 255);
+  ASSERT_EQ(testColor.g, 128);
+  ASSERT_EQ(testColor.b, 0);
+  ASSERT_EQ(testColor.a, 255);
+
+  const util::sRGBColor testColor2(255, 128, 0, 255);
+
+  ASSERT_EQ(testColor2.r, 255);
+  ASSERT_EQ(testColor2.g, 128);
+  ASSERT_EQ(testColor2.b, 0);
+  ASSERT_EQ(testColor2.a, 255);
+}
+
+TEST(sRGBColorTest, toRGBA32) {
+  const util::sRGBColor testColor(255, 128, 0);
+  ASSERT_EQ(testColor.toRGBA32(), 0xff8000ff);
+}
+
+TEST(sRGBColorTest, fromRGBA32) {
+  const util::sRGBColor testColor(0xff8000ff);
+  ASSERT_EQ(testColor.r, 255);
+  ASSERT_EQ(testColor.g, 128);
+  ASSERT_EQ(testColor.b, 0);
+  ASSERT_EQ(testColor.a, 255);
 }
 } // namespace igl::tests
