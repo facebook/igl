53044bd05f6d7e817cc320f784f7e0793d33ce9d
Francois Coulombe <frankyc@meta.com>
2024-10-16 16:38:10 -0700

    Create sRGBColor struct to help with sRGB encoding
    Reviewed By: maliarov, mmaurer
    
    Differential Revision: D64466719
    
    fbshipit-source-id: 199e9221461da15b7b1c607b1a756037a7d562c6

--- src/igl/tests/util/Color.h
+++ src/igl/tests/util/Color.h
@@ -6,6 +6,7 @@
  */
 
 #pragma once
+#include <cstdint>
 #include <glm/gtc/color_space.hpp>
 
 namespace igl::tests::util {
@@ -26,4 +27,74 @@ glm::vec<L, T, Q> convertLinearToSRGB(const glm::vec<L, T, Q>& linearColor,
                                       double gamma = kDefaultGamma) {
   return glm::vec<L, T, Q>(glm::convertLinearToSRGB(glm::vec<L, double, Q>(linearColor), gamma));
 }
+
+/**
+ * @brief struct to represent a 32bits sRGB color value. It is assumed that the rgb colors are gamma
+ * compressed using the sRGB transfer function and the alpha is linear.
+ */
+struct sRGBColor {
+  uint8_t r;
+  uint8_t g;
+  uint8_t b;
+  uint8_t a;
+
+  /**
+   * @brief Constructor ingesting red, green, blue. alpha is assumed to be 255.
+   * @remark the red green and blue are assumed to be gamma compressed using the sRGB transfer
+   * function.
+   *
+   * @param red - red channel
+   * @param green - green channel
+   * @param blue - blue channel
+   */
+  constexpr sRGBColor(uint8_t red, uint8_t green, uint8_t blue) :
+    r(red), g(green), b(blue), a(255) {}
+
+  /**
+   * @brief Constructor ingesting red, green, blue, alpha.
+   * @remark the red green and blue are assumed to be gamma compressed using the sRGB transfer
+   * function and the alpha is linear..
+   *
+   * @param red - red channel
+   * @param green - green channel
+   * @param blue - blue channel
+   * @param alpha - alpha channel
+   */
+  constexpr sRGBColor(uint8_t red, uint8_t green, uint8_t blue, uint8_t alpha) :
+    r(red), g(green), b(blue), a(alpha) {}
+
+  /**
+   * @brief Constructor ingesting red, green, blue, alpha.
+   *
+   * @param rgba - 32bits integer containing concatenated 8 bits for red, green, blue and alpha.
+   * @remark the red green and blue are assumed to be gamma compressed using the sRGB transfer
+   * function and the alpha is linear.
+   */
+  sRGBColor(uint32_t rgba) {
+    fromRGBA32(rgba);
+  }
+
+  /**
+   * @brief Encode the rgba values into a single 32bits integer.
+   * @return 32bits integer containing the rgba values.
+   * @remark the red green and blue are assumed to be gamma compressed using the sRGB transfer
+   * function and the alpha is linear.
+   */
+  constexpr uint32_t toRGBA32() const {
+    return (r << 24) | (g << 16) | (b << 8) | a;
+  }
+
+  /**
+   * @brief Decode the rgba values into a single 32bits integer.
+   * @param rgba - 32bits integer containing the rgba values.
+   * @remark the rgba are assumed to be gamma compressed using the sRGB transfer function and the
+   * alpha is linear.
+   */
+  constexpr void fromRGBA32(uint32_t rgba) {
+    r = (rgba >> 24);
+    g = (rgba >> 16) & 0xff;
+    b = (rgba >> 8) & 0xff;
+    a = (rgba >> 0) & 0xff;
+  }
+};
 } // namespace igl::tests::util
