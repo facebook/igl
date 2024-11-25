dc9fb2896b29d29450be893b4260a34c624190fc
vinsentli <vinsentli@tencent.com>
2024-11-25 14:50:39 +0800

    revert

--- build/android/app/src/main/java/com/facebook/igl/sample/SampleLib.java
+++ build/android/app/src/main/java/com/facebook/igl/sample/SampleLib.java
@@ -16,7 +16,7 @@ import android.view.Surface;
 public class SampleLib {
 
   static {
-    System.loadLibrary("NanovgSession");
+    System.loadLibrary("ColorSession");
   }
 
   // Must match igl/Common.h
