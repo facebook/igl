12603064b8ad3e73c5a17e4b95c3b393b5ecbf2d
vinsentli <vinsentli@tencent.com>
2024-12-22 19:45:02 +0800

    disable stencil buffer on Android OpenGLES.

--- build/android/app/src/main/java/com/facebook/igl/sample/SampleActivity.java
+++ build/android/app/src/main/java/com/facebook/igl/sample/SampleActivity.java
@@ -69,7 +69,7 @@ public class SampleActivity extends Activity implements View.OnClickListener {
       } else if (mConfigs[i].version.flavor == SampleLib.BackendFlavor.OpenGL_ES) {
         backendView =
             new SampleView(
-                getApplication(), mConfigs[i].version, mConfigs[i].swapchainColorTextureFormat, true);
+                getApplication(), mConfigs[i].version, mConfigs[i].swapchainColorTextureFormat);
         ((SampleView) backendView).onPause();
       }
 
