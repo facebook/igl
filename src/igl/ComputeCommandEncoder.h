b9a875d4c493d4f3d68e628b24f0bff2cf8ffabb
Sergey Kosarevsky <sergkosarevsky@meta.com>
2025-02-25 00:43:36 -0800

    igl | Remove stub
    Reviewed By: AmesingFlank
    
    Differential Revision: D70110000
    
    fbshipit-source-id: a487c6cddc6e513435c7ffcb04fb294239442af9

--- src/igl/ComputeCommandEncoder.h
+++ src/igl/ComputeCommandEncoder.h
@@ -62,12 +62,7 @@ class IComputeCommandEncoder : public ICommandEncoder {
    * @param texture The texture to set in the texture argument table.
    */
   virtual void bindTexture(uint32_t index, ITexture* texture) = 0;
-  virtual void bindImageTexture(uint32_t index, ITexture* texture, TextureFormat format) {
-    // @fb-only
-    (void)index;
-    (void)texture;
-    (void)format;
-  }
+  virtual void bindImageTexture(uint32_t index, ITexture* texture, TextureFormat format) = 0;
   /**
    * @brief Sets a buffer for the compute function
    *
