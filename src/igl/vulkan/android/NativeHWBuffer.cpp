4ea0a6ec2bfe4ba854d6b4cf45930acc9a7bba06
vinsentli <vinsentli@tencent.com>
2025-02-27 19:27:38 -0800

    igl | vulkan | Fix NativeHWTextureBuffer::upload() failed because of the attributes such as .type and .storage in desc_ is missed. (#250)
    Summary: Pull Request resolved: https://github.com/facebook/igl/pull/250
    
    Reviewed By: corporateshark, rameshviswanathan
    
    Differential Revision: D70328921
    
    Pulled By: francoiscoulombe
    
    fbshipit-source-id: e7af9d0485c9cfaeea46e8651a823bf5105fe82d

--- src/igl/vulkan/android/NativeHWBuffer.cpp
+++ src/igl/vulkan/android/NativeHWBuffer.cpp
@@ -52,10 +52,6 @@ Result NativeHWTextureBuffer::createTextureInternal(AHardwareBuffer* hwBuffer) {
   }
   AHardwareBuffer_Desc hwbDesc;
   AHardwareBuffer_describe(hwBuffer, &hwbDesc);
-  TextureDesc desc;
-  desc.width = hwbDesc.width;
-  desc.height = hwbDesc.height;
-  desc.usage = ::igl::android::getIglBufferUsage(hwbDesc.usage);
 
   auto& ctx = device_.getVulkanContext();
   auto device = device_.getVulkanContext().getVkDevice();
@@ -101,26 +97,24 @@ Result NativeHWTextureBuffer::createTextureInternal(AHardwareBuffer* hwBuffer) {
       .handleTypes = VK_EXTERNAL_MEMORY_HANDLE_TYPE_ANDROID_HARDWARE_BUFFER_BIT_ANDROID,
   };
 
-  desc.format = igl::vulkan::vkFormatToTextureFormat(ahb_format_props.format);
-
   VkImage vk_image;
 
-  VkImageCreateInfo vk_image_info = {.sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO,
-                                     .pNext = &external_memory_image_info,
-                                     .flags = create_flags,
-                                     .imageType = VK_IMAGE_TYPE_2D,
-                                     .format = ahb_format_props.format,
-                                     .extent =
-                                         VkExtent3D{(uint32_t)desc.width, (uint32_t)desc.height, 1},
-                                     .mipLevels = 1,
-                                     .arrayLayers = 1,
-                                     .samples = VK_SAMPLE_COUNT_1_BIT,
-                                     .tiling = VK_IMAGE_TILING_OPTIMAL,
-                                     .usage = usage_flags,
-                                     .sharingMode = VK_SHARING_MODE_EXCLUSIVE,
-                                     .queueFamilyIndexCount = 0,
-                                     .pQueueFamilyIndices = nullptr,
-                                     .initialLayout = VK_IMAGE_LAYOUT_UNDEFINED};
+  VkImageCreateInfo vk_image_info = {
+      .sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO,
+      .pNext = &external_memory_image_info,
+      .flags = create_flags,
+      .imageType = VK_IMAGE_TYPE_2D,
+      .format = ahb_format_props.format,
+      .extent = VkExtent3D{(uint32_t)hwbDesc.width, (uint32_t)hwbDesc.height, 1},
+      .mipLevels = 1,
+      .arrayLayers = 1,
+      .samples = VK_SAMPLE_COUNT_1_BIT,
+      .tiling = VK_IMAGE_TILING_OPTIMAL,
+      .usage = usage_flags,
+      .sharingMode = VK_SHARING_MODE_EXCLUSIVE,
+      .queueFamilyIndexCount = 0,
+      .pQueueFamilyIndices = nullptr,
+      .initialLayout = VK_IMAGE_LAYOUT_UNDEFINED};
   // Create Vk Image.
   VK_ASSERT(ctx.vf_.vkCreateImage(device, &vk_image_info, nullptr, &vk_image));
 
@@ -258,7 +252,7 @@ Result NativeHWTextureBuffer::createTextureInternal(AHardwareBuffer* hwBuffer) {
     return Result(Result::Code::RuntimeError, "Failed to create vulkan texture");
   }
 
-  desc_ = std::move(desc);
+  desc_ = textureDesc_;
   texture_ = std::move(vkTexture);
 
   return Result{Result::Code::Ok};
