daa9a86e7509f54a79a92c9e4fe598a83f883ed0
Francois Coulombe <frankyc@meta.com>
2025-02-27 19:27:38 -0800

    re-enable hwbuffer vulkan tests
    Reviewed By: EricGriffith
    
    Differential Revision: D70336011
    
    fbshipit-source-id: 99224cf55db5a4a0d10bfa36e834c57af78694b1

--- src/igl/tests/android/NativeHWBuffer.cpp
+++ src/igl/tests/android/NativeHWBuffer.cpp
@@ -386,8 +386,7 @@ TEST_F(NativeHWBufferTextureTestOpenGL3, SharedMemoryTexture) {
       *iglDev_, *cmdQueue_, texture, pixels_.data(), "HWBufferTextureOpenGL3");
 }
 
-// @fb-only
-using NativeHWBufferTextureTestVulkan = NativeHWBufferTextureTest<{igl::BackendType::Vulkan}>;
+using NativeHWBufferTextureTestVulkan = NativeHWBufferTextureTest<igl::BackendType::Vulkan>;
 
 TEST_F(NativeHWBufferTextureTestVulkan, SharedMemoryTexture) {
   Result outResult;
@@ -401,7 +400,6 @@ TEST_F(NativeHWBufferTextureTestVulkan, SharedMemoryTexture) {
   util::validateUploadedTexture(
       *iglDev_, *cmdQueue_, texture, pixels_.data(), "HWBufferTextureVulkan");
 }
-// @fb-only
 
 } // namespace igl::tests
 
