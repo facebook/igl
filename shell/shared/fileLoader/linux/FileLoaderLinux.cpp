94b5e35fa5d8e113ce65e718cb86deeb00c49611
Oleg Gordynchuk <ogordynchuk@meta.com>
2025-03-07 14:13:17 -0800

    fix resource load error in stellar tests
    Reviewed By: rudybear
    
    Differential Revision: D70720148
    
    fbshipit-source-id: ab8f0b66e01fd5efd4991720a40dbaecfafa85e8

--- shell/shared/fileLoader/linux/FileLoaderLinux.cpp
+++ shell/shared/fileLoader/linux/FileLoaderLinux.cpp
@@ -94,6 +94,7 @@ std::string FileLoaderLinux::fullPath(const std::string& fileName) const {
       // @fb-only
       // @fb-only
       // @fb-only
+      // @fb-only
   };
 
   // find folders somewhere above our current directory
