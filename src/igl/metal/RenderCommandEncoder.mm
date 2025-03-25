ce8793718520686af4c27e73358787ac77a19c72
vinsentli <vinsentli@tencent.com>
2025-03-25 10:27:02 +0800

    support instanceCount on iOS 8.0

--- src/igl/metal/RenderCommandEncoder.mm
+++ src/igl/metal/RenderCommandEncoder.mm
@@ -384,7 +384,14 @@ void RenderCommandEncoder::draw(size_t vertexCount,
                 baseInstance:baseInstance];
 #if IGL_PLATFORM_IOS
   } else {
-    [encoder_ drawPrimitives:metalPrimitive_ vertexStart:firstVertex vertexCount:vertexCount];
+    if (!baseInstance) {
+      [encoder_ drawPrimitives:metalPrimitive_
+                   vertexStart:firstVertex
+                   vertexCount:vertexCount
+                 instanceCount:instanceCount];
+    } else {
+      IGL_DEBUG_ASSERT_NOT_REACHED();
+    }
   }
 #endif // IGL_PLATFORM_IOS
 }
@@ -417,11 +424,16 @@ void RenderCommandEncoder::drawIndexed(size_t indexCount,
                        baseInstance:baseInstance];
 #if IGL_PLATFORM_IOS
   } else {
-    [encoder_ drawIndexedPrimitives:metalPrimitive_
-                         indexCount:indexCount
-                          indexType:indexType_
-                        indexBuffer:indexBuffer_
-                  indexBufferOffset:indexBufferOffset_ + indexOffsetBytes];
+    if (!baseInstance) {
+      [encoder_ drawIndexedPrimitives:metalPrimitive_
+                           indexCount:indexCount
+                            indexType:indexType_
+                          indexBuffer:indexBuffer_
+                    indexBufferOffset:indexBufferOffset_ + indexOffsetBytes
+                        instanceCount:instanceCount];
+    } else {
+      IGL_DEBUG_ASSERT_NOT_REACHED();
+    }
   }
 #endif // IGL_PLATFORM_IOS
 }
