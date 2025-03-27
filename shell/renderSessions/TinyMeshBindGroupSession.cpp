80b6f8c91ddfc2f8af2408c8c6e8aa103e7520ac
generatedunixname89002005287564 <generatedunixname89002005287564@meta.com>
2025-03-27 05:54:10 -0700

    fbsource//xplat/graphics/igl/public/shell/renderSessions
    Reviewed By: nlutsenko
    
    Differential Revision: D71955010
    
    fbshipit-source-id: 23a3433544d53d03a240043667d08ea74b6b8f25

--- shell/renderSessions/TinyMeshBindGroupSession.cpp
+++ shell/renderSessions/TinyMeshBindGroupSession.cpp
@@ -131,9 +131,8 @@ const VertexPosUvw vertexData0[] = {
     {{-half, +half, +half}, {0.0, 1.0, 1.0}, {1, 1}}, // 23
 };
 
-static uint16_t indexData[] = {0,  1,  2,  2,  3,  0,  4,  5,  6,  6,  7,  4,
-                               8,  9,  10, 10, 11, 8,  12, 13, 14, 14, 15, 12,
-                               16, 17, 18, 18, 19, 16, 20, 21, 22, 22, 23, 20};
+uint16_t indexData[] = {0,  1,  2,  2,  3,  0,  4,  5,  6,  6,  7,  4,  8,  9,  10, 10, 11, 8,
+                        12, 13, 14, 14, 15, 12, 16, 17, 18, 18, 19, 16, 20, 21, 22, 22, 23, 20};
 
 UniformsPerFrame perFrame;
 UniformsPerObject perObject[kNumCubes];
