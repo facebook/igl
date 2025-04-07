3b213e2c56817c1b99f2d777f308d9ab255d9f0e
vinsentli <vinsentli@tencent.com>
2025-04-07 11:17:08 +0800

    drawPrimitives @available(iOS 16

--- src/igl/metal/RenderCommandEncoder.mm
+++ src/igl/metal/RenderCommandEncoder.mm
@@ -375,7 +375,7 @@ void RenderCommandEncoder::draw(size_t vertexCount,
   getCommandBuffer().incrementCurrentDrawCount();
   IGL_DEBUG_ASSERT(encoder_);
 #if IGL_PLATFORM_IOS
-  if (@available(iOS 9, *)) {
+  if (@available(iOS 16, *)) {
 #endif // IGL_PLATFORM_IOS
     [encoder_ drawPrimitives:metalPrimitive_
                  vertexStart:firstVertex
@@ -412,7 +412,7 @@ void RenderCommandEncoder::drawIndexed(size_t indexCount,
       static_cast<size_t>(firstIndex) * (indexType_ == MTLIndexTypeUInt32 ? 4u : 2u);
 
 #if IGL_PLATFORM_IOS
-  if (@available(iOS 9, *)) {
+  if (@available(iOS 16, *)) {
 #endif // IGL_PLATFORM_IOS
     [encoder_ drawIndexedPrimitives:metalPrimitive_
                          indexCount:indexCount
