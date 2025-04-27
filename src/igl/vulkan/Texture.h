1fe6e486b7db173ac6bc59ae221054e902d98ba1
generatedunixname89002005287564 <generatedunixname89002005287564@meta.com>
2025-04-26 23:45:38 -0700

    fbsource//xplat/graphics/igl/public/src/igl/vulkan [A] [A]
    Reviewed By: corporateshark
    
    Differential Revision: D73723157
    
    fbshipit-source-id: f6c9a2b461e9c3396c3df74cc322def951f4a31c

--- src/igl/vulkan/Texture.h
+++ src/igl/vulkan/Texture.h
@@ -18,7 +18,6 @@
 namespace igl::vulkan {
 
 class Device;
-class VulkanImage;
 class VulkanTexture;
 class PlatformDevice;
 
@@ -30,10 +29,10 @@ class Texture : public ITexture {
  public:
   /// @brief Initializes an instance of the class, but does not create the resource on the device
   /// until `create()` is called
-  Texture(igl::vulkan::Device& device, TextureFormat format);
+  Texture(Device& device, TextureFormat format);
 
   /// @brief Initializes an instance of the class with an existing VulkanTexture object.
-  Texture(igl::vulkan::Device& device, std::shared_ptr<VulkanTexture> vkTexture, TextureDesc desc) :
+  Texture(Device& device, std::shared_ptr<VulkanTexture> vkTexture, TextureDesc desc) :
     Texture(device, desc.format) {
     texture_ = std::move(vkTexture);
     desc_ = std::move(desc);
@@ -83,7 +82,7 @@ class Texture : public ITexture {
   void clearColorTexture(const igl::Color& rgba);
 
  protected:
-  igl::vulkan::Device& device_;
+  Device& device_;
   TextureDesc desc_;
 
   std::shared_ptr<VulkanTexture> texture_;
