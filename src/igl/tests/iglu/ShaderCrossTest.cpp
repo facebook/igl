335dd0e71cc9d43336784882c3f736da23968b47
Roman Kuznetsov <rokuz@meta.com>
2025-05-13 10:37:43 -0700

    Add CrossCompile test for ShaderCross IGLU module
    Reviewed By: maliarov
    
    Differential Revision:
    D74651736
    
    Privacy Context Container: L1159089
    
    fbshipit-source-id: 74efe353dd9b30cffdefd05f7e370f9e3ace2d15

--- src/igl/tests/iglu/ShaderCrossTest.cpp
+++ src/igl/tests/iglu/ShaderCrossTest.cpp
@@ -11,6 +11,60 @@
 
 namespace igl::tests {
 
+namespace {
+[[nodiscard]] const char* getVulkanFragmentShaderSource() {
+  return R"(#version 450
+              precision highp float;
+              precision highp sampler2D;
+  
+              layout(location = 0) in vec3 uvw;
+              layout(location = 1) in vec3 color;
+              layout(set = 0, binding = 0) uniform sampler2D inputImage;
+              layout(location = 0) out vec4 fragmentColor;
+  
+              void main() {
+                fragmentColor = texture(inputImage, uvw.xy) * vec4(color, 1.0);
+              })";
+}
+
+[[nodiscard]] std::string getVertexShaderProlog(bool stereoRendering) {
+  return stereoRendering ? R"(#version 450
+      #extension GL_OVR_multiview2 : require
+      layout(num_views = 2) in;
+      precision highp float;
+  
+      #define VIEW_ID int(gl_ViewID_OVR)
+    )"
+                         : R"(#version 450
+      precision highp float;
+  
+      #define VIEW_ID perFrame.viewId
+    )";
+}
+
+[[nodiscard]] std::string getVulkanVertexShaderSource(bool stereoRendering) {
+  return getVertexShaderProlog(stereoRendering) + R"(
+              layout (set = 1, binding = 1, std140) uniform PerFrame {
+                mat4 modelMatrix;
+                mat4 viewProjectionMatrix[2];
+                float scaleZ;
+                int viewId;
+              } perFrame;
+  
+              layout(location = 0) in vec3 position;
+              layout(location = 1) in vec3 uvw_in;
+              layout(location = 0) out vec3 uvw;
+              layout(location = 1) out vec3 color;
+  
+              void main() {
+                mat4 mvpMatrix = perFrame.viewProjectionMatrix[VIEW_ID] * perFrame.modelMatrix;
+                gl_Position = mvpMatrix * vec4(position, 1.0);
+                uvw = vec3(uvw_in.x, uvw_in.y, (uvw_in.z - 0.5) * perFrame.scaleZ + 0.5);
+                color = vec3(1.0, 1.0, 0.0);
+              })";
+}
+} // namespace
+
 class ShaderCrossTest : public ::testing::Test {
  public:
   ShaderCrossTest() = default;
@@ -44,6 +98,33 @@ TEST_F(ShaderCrossTest, EntryPointName) {
     EXPECT_EQ(shaderCross.entryPointName(igl::ShaderStage::Vertex), "main");
   } else {
     EXPECT_EQ(shaderCross.entryPointName(igl::ShaderStage::Vertex), "");
+  }
+}
+
+TEST_F(ShaderCrossTest, CrossCompile) {
+  const iglu::ShaderCross shaderCross(*iglDev_);
+  Result res;
+  if (iglDev_->getBackendType() == igl::BackendType::Metal) {
+    const auto vs = shaderCross.crossCompileFromVulkanSource(
+        getVulkanVertexShaderSource(false).c_str(), igl::ShaderStage::Vertex, &res);
+    EXPECT_TRUE(res.isOk());
+    EXPECT_TRUE(!vs.empty());
+
+    const auto fs = shaderCross.crossCompileFromVulkanSource(
+        getVulkanFragmentShaderSource(), igl::ShaderStage::Fragment, &res);
+    EXPECT_TRUE(res.isOk());
+    EXPECT_TRUE(!fs.empty());
+
+  } else if (iglDev_->getBackendType() == igl::BackendType::OpenGL) {
+    const auto vs = shaderCross.crossCompileFromVulkanSource(
+        getVulkanVertexShaderSource(true).c_str(), igl::ShaderStage::Vertex, &res);
+    EXPECT_TRUE(res.isOk());
+    EXPECT_TRUE(!vs.empty());
+
+    const auto fs = shaderCross.crossCompileFromVulkanSource(
+        getVulkanFragmentShaderSource(), igl::ShaderStage::Fragment, &res);
+    EXPECT_TRUE(res.isOk());
+    EXPECT_TRUE(!fs.empty());
   }
 }
 
