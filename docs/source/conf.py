baea506b3e557ae99ce94d3aad5f2bdbc9509d8d
Roman Kuznetsov <rokuz@meta.com>
2025-05-13 10:37:43 -0700

    Exclude docs generator from code coverage
    Reviewed By: maliarov
    
    Differential Revision:
    D74654049
    
    Privacy Context Container: L1159089
    
    fbshipit-source-id: 5e26e5c10fac3a6a13ffe170e14bb93998ff5ced

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,6 +3,8 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+# @fb-only
+
 # # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
