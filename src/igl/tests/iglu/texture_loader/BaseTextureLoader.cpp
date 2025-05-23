4f249e384ef02a9ce47af2916f15f138a107d8bf
Roman Kuznetsov <rokuz@meta.com>
2025-05-23 07:14:16 -0700

    Add base tests for TextureLoader IGLU module
    Reviewed By: maliarov
    
    Differential Revision:
    D75285005
    
    Privacy Context Container: L1159089
    
    fbshipit-source-id: 67400f6e36a878aede8090dd904aaf13d24bc5c8

---
+++ src/igl/tests/iglu/texture_loader/BaseTextureLoader.cpp
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include "../../data/TextureData.h"
+#include "../../util/Common.h"
+
+#include <IGLU/texture_loader/ITextureLoader.h>
+#include <gtest/gtest.h>
+#include <igl/Common.h>
+
+namespace igl::tests {
+
+class TestTextureLoader : public iglu::textureloader::ITextureLoader {
+ public:
+  TestTextureLoader(iglu::textureloader::DataReader reader,
+                    TextureDesc::TextureUsage usage) noexcept :
+    iglu::textureloader::ITextureLoader(std::move(reader), usage) {}
+
+  TextureDesc& descriptorRef() noexcept {
+    return mutableDescriptor();
+  }
+};
+
+class BaseTextureLoaderTest : public ::testing::Test {
+ private:
+ public:
+  BaseTextureLoaderTest() = default;
+  ~BaseTextureLoaderTest() override = default;
+
+  //
+  // SetUp()
+  // Create device, commandQueue
+  //
+  void SetUp() override {
+    setDebugBreakEnabled(false);
+    util::createDeviceAndQueue(iglDev_, cmdQueue_);
+    ASSERT_TRUE(iglDev_ != nullptr);
+    ASSERT_TRUE(cmdQueue_ != nullptr);
+  }
+
+  void TearDown() override {}
+
+  // Member variables
+ protected:
+  std::shared_ptr<IDevice> iglDev_;
+  std::shared_ptr<ICommandQueue> cmdQueue_;
+};
+
+TEST_F(BaseTextureLoaderTest, CheckCapabilities) {
+  Result result;
+  auto dataReader = iglu::textureloader::DataReader::tryCreate(
+      reinterpret_cast<const uint8_t*>(data::texture::TEX_RGBA_2x2),
+      sizeof(data::texture::TEX_RGBA_2x2),
+      &result);
+  ASSERT_TRUE(result.isOk());
+  ASSERT_TRUE(dataReader.has_value());
+
+  TestTextureLoader loader(*dataReader, TextureDesc::TextureUsageBits::Sampled);
+  loader.descriptorRef().format = TextureFormat::RGBA_UNorm8;
+
+  ASSERT_TRUE(loader.isSupported(*iglDev_));
+
+  ASSERT_FALSE(loader.canUploadSourceData());
+  ASSERT_FALSE(loader.canUseExternalMemory());
+  ASSERT_FALSE(loader.shouldGenerateMipmaps());
+}
+} // namespace igl::tests
