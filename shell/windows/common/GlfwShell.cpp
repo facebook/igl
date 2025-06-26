4ca99d713378bcfab7307e2e2be533e7781769aa
Sergey Kosarevsky <sergkosarevsky@meta.com>
2025-06-26 00:59:35 -0700

    igl | shell | Add command line option `--screenshot-file <filename>`
    Reviewed By: francoiscoulombe
    
    Differential Revision: D77343290
    
    fbshipit-source-id: e48d528aa15eb87018ab12a69d53a995f888c21d

--- shell/windows/common/GlfwShell.cpp
+++ shell/windows/common/GlfwShell.cpp
@@ -197,6 +197,12 @@ bool GlfwShell::initialize(int argc,
   for (int i = 1; i < argc; i++) {
     if (!strcmp(argv[i], "--headless")) {
       shellParams_.isHeadless = true;
+    } else if (!strcmp(argv[i], "--screenshot-file")) {
+      if (i + 1 < argc) {
+        shellParams_.screenshotFileName = argv[++i];
+      } else {
+        IGL_LOG_ERROR("Specify a file name for `--screenshot-file <filename>`\n");
+      }
     }
   }
 
