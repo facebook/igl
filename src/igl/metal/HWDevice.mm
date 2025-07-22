445fc669ec442029eeccffb61d2091e77630cf17
Alexey Medvedev <amedvedev@meta.com>
2025-07-22 01:49:05 -0700

    Fix performance-no-int-to-ptr in HWDevice.mm
    Reviewed By: nlutsenko, dmannemela
    
    Differential Revision: D78714617
    
    fbshipit-source-id: 59b4bd6478e0d43ab9eb76309a289e29d9903f0b

--- src/igl/metal/HWDevice.mm
+++ src/igl/metal/HWDevice.mm
@@ -139,7 +139,8 @@ std::unique_ptr<IDevice> HWDevice::create(const HWDeviceDesc& desc, Result* outR
     return nullptr;
   }
 
-  return createWithMTLDevice((__bridge id<MTLDevice>)(void*)desc.guid, outResult);
+  return createWithMTLDevice((__bridge id<MTLDevice>)(void*)desc.guid,
+                             outResult); // NOLINT(performance-no-int-to-ptr)
 }
 
 std::unique_ptr<IDevice> HWDevice::createWithSystemDefaultDevice(Result* outResult) {
