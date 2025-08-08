d3bb18982641efd0bd786340a82ab9b4ea952acf
generatedunixname537391475639613 <noreply+537391475639613@fb.com>
2025-08-08 11:32:08 -0700

    xplat/graphics/igl/public/src/igl/tests/iglu/texture_loader/StbPngTextureLoaderTest.cpp
    Reviewed By: corporateshark
    
    Differential Revision: D79897580
    
    fbshipit-source-id: 301492ff65a8b7644f5ef26f441741218be3bbee

--- src/igl/tests/iglu/texture_loader/StbPngTextureLoaderTest.cpp
+++ src/igl/tests/iglu/texture_loader/StbPngTextureLoaderTest.cpp
@@ -10,7 +10,6 @@
 #include <IGLU/texture_loader/stb_png/Header.h>
 #include <IGLU/texture_loader/stb_png/TextureLoaderFactory.h>
 #include <cstring>
-#include <numeric>
 #include <vector>
 
 namespace igl::tests::stb::png {
