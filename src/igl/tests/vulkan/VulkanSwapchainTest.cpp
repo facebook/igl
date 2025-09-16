806b75bd6dbfba6e7f57a8f7557979d08fba1419
Sinclair Yeh <syeh1@meta.com>
2025-09-16 13:20:35 -0700

    Make tool stop complaining
    Reviewed By: EricGriffith
    
    Differential Revision: D82562730
    
    fbshipit-source-id: 121a25fe1a017e8efc26ab3c4ce919d744b4773f

--- src/igl/tests/vulkan/VulkanSwapchainTest.cpp
+++ src/igl/tests/vulkan/VulkanSwapchainTest.cpp
@@ -12,7 +12,7 @@
 #include <gtest/gtest.h>
 #include <memory>
 
-#include <igl/tests/util/device/vulkan/TestDevice.h>
+#include <igl/tests/util/device/vulkan/TestDevice.h> // IWYU pragma: export
 #include <igl/vulkan/Device.h>
 #include <igl/vulkan/VulkanContext.h>
 #include <igl/vulkan/VulkanSwapchain.h>
