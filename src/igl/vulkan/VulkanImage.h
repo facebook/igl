d24edea3c643f22523efd060383dd584db4af127
Sergey Kosarevsky <sergkosarevsky@meta.com>
2025-10-21 12:53:12 -0700

    igl | vulkan | Remove the `VkDevice` parameter from `VulkanImage` constructors (part 1)
    Reviewed By: dmannemela, metarokuz
    
    Differential Revision: D84877198
    
    fbshipit-source-id: 36e12c3b29e2e00ab653d0afb2751004a39149f0

--- src/igl/vulkan/VulkanImage.h
+++ src/igl/vulkan/VulkanImage.h
@@ -42,7 +42,6 @@ class VulkanImage final {
    * the constructor will assign it to the `VkImage` object. No other Vulkan functions are called
    */
   VulkanImage(const VulkanContext& ctx,
-              VkDevice device,
               VkImage image,
               const char* debugName = nullptr,
               VkImageUsageFlags usageFlags = 0,
@@ -60,7 +59,6 @@ class VulkanImage final {
    * the constructor will assign it to the `VkImage` object. No other Vulkan functions are called
    */
   VulkanImage(const VulkanContext& ctx,
-              VkDevice device,
               VkImage image,
               const VulkanImageCreateInfo& createInfo,
               const char* debugName = nullptr);
@@ -77,7 +75,6 @@ class VulkanImage final {
    * it is memory mapped until the object's destruction.
    */
   VulkanImage(const VulkanContext& ctx,
-              VkDevice device,
               VkExtent3D extent,
               VkImageType type,
               VkFormat format,
@@ -197,7 +194,6 @@ class VulkanImage final {
    * On Linux/Android, the exported file descriptor will be stored in `exportedFd_`.
    */
   static VulkanImage createWithExportMemory(const VulkanContext& ctx,
-                                            VkDevice device,
                                             VkExtent3D extent,
                                             VkImageType type,
                                             VkFormat format,
