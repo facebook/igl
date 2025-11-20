1016e2d9cf4d0887f10387092d277c64ff9bc63a
generatedunixname537391475639613 <noreply+537391475639613@fb.com>
2025-11-20 07:40:38 -0800

    xplat/graphics/igl/public/samples/ios/snapshot_test_support/IGLSnapshotTestCase.mm
    Reviewed By: Daniel9z
    
    Differential Revision: D87422294
    
    fbshipit-source-id: 04bbc9ca42a763573df75df33e7cdaeb537c82ef

--- samples/ios/snapshot_test_support/IGLSnapshotTestCase.mm
+++ samples/ios/snapshot_test_support/IGLSnapshotTestCase.mm
@@ -20,68 +20,68 @@
 #include <igl/opengl/ios/HWDevice.h>
 
 @implementation IGLSnapshotTestCase {
-  std::shared_ptr<igl::IDevice> _device;
-  std::shared_ptr<igl::ICommandQueue> _commandQueue;
-  std::shared_ptr<igl::IFramebuffer> _framebuffer;
-  igl::BackendType _backendType;
-  std::shared_ptr<iglu::kit::IRenderable> _renderable;
+  std::shared_ptr<igl::IDevice> device;
+  std::shared_ptr<igl::ICommandQueue> commandQueue;
+  std::shared_ptr<igl::IFramebuffer> framebuffer;
+  igl::BackendType backendType;
+  std::shared_ptr<iglu::kit::IRenderable> renderable;
 }
 
 - (void)setUp {
   [super setUp];
 
-  _renderable = std::make_shared<iglu::kit::TinyRenderable>();
-  _backendType = igl::BackendType::OpenGL;
+  renderable = std::make_shared<iglu::kit::TinyRenderable>();
+  backendType = igl::BackendType::OpenGL;
   [self initIGL];
 }
 
 - (void)initIGL {
   igl::HWDeviceQueryDesc queryDesc(igl::HWDeviceType::DiscreteGpu);
-  if (_backendType == igl::BackendType::Metal) {
+  if (backendType == igl::BackendType::Metal) {
     igl::metal::HWDevice hwDevice;
     auto hwDevices = hwDevice.queryDevices(queryDesc, nullptr);
-    _device = hwDevice.create(hwDevices[0], nullptr);
-  } else if (_backendType == igl::BackendType::OpenGL) {
+    device = hwDevice.create(hwDevices[0], nullptr);
+  } else if (backendType == igl::BackendType::OpenGL) {
     igl::opengl::ios::HWDevice hwDevice;
-    _device = hwDevice.create(nullptr);
+    device = hwDevice.create(nullptr);
   }
 
   igl::Result result;
-  igl::DeviceScope scope(*_device);
+  igl::DeviceScope scope(*device);
   igl::CommandQueueDesc desc{};
-  _commandQueue = _device->createCommandQueue(desc, &result);
+  commandQueue = device->createCommandQueue(desc, &result);
   IGL_DEBUG_ASSERT(
       result.isOk(), "Simple sample create command queue failed: %s\n", result.message.c_str());
 }
 
 - (std::shared_ptr<igl::ITexture>)createOrUpdateFramebuffer {
-  IGL_DEBUG_ASSERT(_device->verifyScope());
+  IGL_DEBUG_ASSERT(device->verifyScope());
 
   auto texDesc = igl::TextureDesc::new2D(
       igl::TextureFormat::RGBA_UNorm8, 720, 1280, igl::TextureDesc::TextureUsageBits::Attachment);
-  std::shared_ptr<igl::ITexture> targetTexture = _device->createTexture(texDesc, nullptr);
+  std::shared_ptr<igl::ITexture> targetTexture = device->createTexture(texDesc, nullptr);
 
-  if (_framebuffer) {
-    _framebuffer->updateDrawable(targetTexture);
+  if (framebuffer) {
+    framebuffer->updateDrawable(targetTexture);
   } else {
     igl::Result result;
     igl::FramebufferDesc framebufferDesc;
     framebufferDesc.colorAttachments[0].texture = targetTexture;
-    _framebuffer = _device->createFramebuffer(framebufferDesc, &result);
+    framebuffer = device->createFramebuffer(framebufferDesc, &result);
     IGL_DEBUG_ASSERT(result.isOk(), "Could not create framebuffer %s\n", result.message.c_str());
 
-    _renderable->initialize(*_device, *_framebuffer);
+    renderable->initialize(*device, *framebuffer);
   }
   return targetTexture;
 }
 
 - (void)render {
-  igl::DeviceScope scope(*_device);
+  igl::DeviceScope scope(*device);
   auto nativeDrawable = [self createOrUpdateFramebuffer];
 
   igl::Result result;
   igl::CommandBufferDesc cbDesc;
-  auto commandBuffer = _commandQueue->createCommandBuffer(cbDesc, &result);
+  auto commandBuffer = commandQueue->createCommandBuffer(cbDesc, &result);
   IGL_DEBUG_ASSERT(result.isOk(), "Could not create cmd buffer %s\n", result.message.c_str());
 
   igl::RenderPassDesc renderPass;
@@ -90,19 +90,19 @@
   renderPass.colorAttachments[0].storeAction = igl::StoreAction::Store;
   renderPass.colorAttachments[0].clearColor = {1.0, 1.0, 1.0, 1.0};
 
-  auto cmds = commandBuffer->createRenderCommandEncoder(renderPass, _framebuffer);
+  auto cmds = commandBuffer->createRenderCommandEncoder(renderPass, framebuffer);
   IGL_DEBUG_BUFFER_LABEL_START(commandBuffer, "draw renderable");
-  _renderable->submit(*cmds);
+  renderable->submit(*cmds);
   IGL_DEBUG_BUFFER_LABEL_END(commandBuffer);
   cmds->endEncoding();
 
-  _commandQueue->submit(*commandBuffer); // Guarantees ordering between command buffers
+  commandQueue->submit(*commandBuffer); // Guarantees ordering between command buffers
 }
 
 - (void)testTinySample {
   [self render];
 
-  auto image = IGLFramebufferToUIImage(*_framebuffer, *_commandQueue, 720, 1280);
+  auto image = IGLFramebufferToUIImage(*framebuffer, *commandQueue, 720, 1280);
   FBRecordSnapshotData([[FBServerSnapshotTestData alloc] initWithSnapshot:image
                                                              coverageInfo:nil
                                                             configuration:nil
