1137efa0a9f1155c5fd2a6ef1717a524bbabcea5
Yonah Karp <yonah@meta.com>
2025-12-17 21:39:51 -0800

    Stellar | Add images for Stellar C++ Screenshot tests
    Reviewed By: agopshi
    
    Differential Revision: D89385245
    
    fbshipit-source-id: 752b781ef848ce161c186967ecbb670c582b6b67

--- shell/shared/fileLoader/linux/FileLoaderLinux.cpp
+++ shell/shared/fileLoader/linux/FileLoaderLinux.cpp
@@ -118,6 +118,8 @@ std::string FileLoaderLinux::fullPath(const std::string& fileName) const {
                                      // @fb-only
                                      // @fb-only
                                      // @fb-only
+                                     // @fb-only
+                                     // @fb-only
   // clang-format on
   // @fb-only
     // @fb-only
