8def1d6107a3fd8641ada5da286751d7bf29e188
Sajan Ghimire <122589374+SajanGhimire1@users.noreply.github.com>
2026-01-14 07:44:40 +0545

    adding null check for fragmentParamBuffer in render()
    Added a check to ensure fragmentParamBuffer_ is valid before binding in the render pass. This prevents potential crashes while keeping all rendering logic and backend support intact.

--- shell/renderSessions/TQMultiRenderPassSession.cpp
+++ shell/renderSessions/TQMultiRenderPassSession.cpp
@@ -23,7 +23,7 @@ static VertexPosUv vertexData0[] = {
     {{-1.0f, 1.0f, 0.0}, {0.0, 1.0}},
     {{1.0f, 1.0f, 0.0}, {1.0, 1.0}},
     {{-1.0f, -1.0, 0.0}, {0.0, 0.0}},
-    {{1.0f, -1.0f, 0.0}, {1.0, 0.0}},
+    {{1.0f, -1.0, 0.0}, {1.0, 0.0}},
 };
 static VertexPosUv vertexData1[] = {
     {{-0.8f, 0.8f, 0.0}, {0.0, 1.0}},
@@ -32,65 +32,76 @@ static VertexPosUv vertexData1[] = {
     {{0.8f, -0.8f, 0.0}, {1.0, 0.0}},
 };
 static uint16_t indexData[] = {
-    0, 1, 2,
-    1, 3, 2,
+    0,
+    1,
+    2,
+    1,
+    3,
+    2,
 };
 
 static std::string getMetalShaderSource() {
   return R"(
-using namespace metal;
+              using namespace metal;
 
-typedef struct { float3 color; } UniformBlock;
+              typedef struct { float3 color; } UniformBlock;
 
-typedef struct {
-  float3 position [[attribute(0)]];
-  float2 uv [[attribute(1)]];
-} VertexIn;
+              typedef struct {
+                float3 position [[attribute(0)]];
+                float2 uv [[attribute(1)]];
+              } VertexIn;
 
-typedef struct {
-  float4 position [[position]];
-  float2 uv;
-} VertexOut;
+              typedef struct {
+                float4 position [[position]];
+                float2 uv;
+              } VertexOut;
 
-vertex VertexOut vertexShader(
-    uint vid [[vertex_id]], constant VertexIn * vertices [[buffer(0)]]) {
-  VertexOut out;
-  out.position = float4(vertices[vid].position, 1.0);
-  out.uv = vertices[vid].uv;
-  return out;
-}
+              vertex VertexOut vertexShader(
+                  uint vid [[vertex_id]], constant VertexIn * vertices [[buffer(0)]]) {
+                VertexOut out;
+                out.position = float4(vertices[vid].position, 1.0);
+                out.uv = vertices[vid].uv;
+                return out;
+              }
 
-fragment float4 fragmentShader(
-    VertexOut IN [[stage_in]],
-    texture2d<float> diffuseTex [[texture(0)]],
-    sampler linearSampler [[sampler(0)]],
-    constant UniformBlock * color [[buffer(0)]]) {
-  float4 tex = diffuseTex.sample(linearSampler, IN.uv);
-  return float4(color->color.r, color->color.g, color->color.b, 1.0) * tex;
-}
-)";
+              fragment float4 fragmentShader(
+                  VertexOut IN [[stage_in]],
+                  texture2d<float> diffuseTex [[texture(0)]],
+                  sampler linearSampler [[sampler(0)]],
+                  constant UniformBlock * color [[buffer(0)]]) {
+                float4 tex = diffuseTex.sample(linearSampler, IN.uv);
+                return float4(color->color.r, color->color.g, color->color.b, 1.0) *
+                      tex;
+              }
+    )";
 }
 
 static std::string getOpenGLVertexShaderSource() {
   return R"(#version 100
-attribute vec3 position;
-attribute vec2 uv_in;
-varying vec2 uv;
-void main() {
-  gl_Position = vec4(position, 1.0);
-  uv = uv_in;
-})";
+                attribute vec3 position;
+                attribute vec2 uv_in;
+
+                varying vec2 uv;
+
+                void main() {
+                  gl_Position = vec4(position, 1.0);
+                  uv = uv_in; // position.xy * 0.5 + 0.5;
+                })";
 }
 
 static std::string getOpenGLFragmentShaderSource() {
   return R"(#version 100
-precision highp float;
-uniform vec3 color;
-uniform sampler2D inputImage;
-varying vec2 uv;
-void main() {
-  gl_FragColor = vec4(color, 1.0) * texture2D(inputImage, uv);
-})";
+                precision highp float;
+
+                uniform vec3 color;
+                uniform sampler2D inputImage;
+
+                varying vec2 uv;
+
+                void main() {
+                  gl_FragColor =
+                      vec4(color, 1.0) * texture2D(inputImage, uv);
+                })";
 }
 
 static std::string getD3D12VertexShaderSource() {
@@ -99,10 +110,12 @@ struct VertexIn {
   float3 position : POSITION;
   float2 uv : TEXCOORD0;
 };
+
 struct VertexOut {
   float4 position : SV_Position;
   float2 uv : TEXCOORD0;
 };
+
 VertexOut main(VertexIn IN) {
   VertexOut OUT;
   OUT.position = float4(IN.position, 1.0);
@@ -117,12 +130,15 @@ static std::string getD3D12FragmentShaderSource() {
 cbuffer UniformBlock : register(b0) {
   float3 color;
 };
+
 Texture2D inputImage : register(t0);
 SamplerState linearSampler : register(s0);
+
 struct VertexOut {
   float4 position : SV_Position;
   float2 uv : TEXCOORD0;
 };
+
 float4 main(VertexOut IN) : SV_Target {
   float4 tex = inputImage.Sample(linearSampler, IN.uv);
   return float4(color.r, color.g, color.b, 1.0) * tex;
@@ -141,17 +157,30 @@ static std::unique_ptr<IShaderStages> getShaderStagesForBackend(IDevice& device)
   case igl::BackendType::Custom:
     IGL_DEBUG_ABORT("IGLSamples not set up for Custom");
     return nullptr;
+  // @fb-only
+    // @fb-only
+    // @fb-only
   case igl::BackendType::Metal:
     return igl::ShaderStagesCreator::fromLibraryStringInput(
         device, getMetalShaderSource().c_str(), "vertexShader", "fragmentShader", "", nullptr);
   case igl::BackendType::OpenGL:
-    return igl::ShaderStagesCreator::fromModuleStringInput(
-        device, getOpenGLVertexShaderSource().c_str(), "main", "",
-        getOpenGLFragmentShaderSource().c_str(), "main", "", nullptr);
+    return igl::ShaderStagesCreator::fromModuleStringInput(device,
+                                                           getOpenGLVertexShaderSource().c_str(),
+                                                           "main",
+                                                           "",
+                                                           getOpenGLFragmentShaderSource().c_str(),
+                                                           "main",
+                                                           "",
+                                                           nullptr);
   case igl::BackendType::D3D12:
-    return igl::ShaderStagesCreator::fromModuleStringInput(
-        device, getD3D12VertexShaderSource().c_str(), "main", "",
-        getD3D12FragmentShaderSource().c_str(), "main", "", nullptr);
+    return igl::ShaderStagesCreator::fromModuleStringInput(device,
+                                                           getD3D12VertexShaderSource().c_str(),
+                                                           "main",
+                                                           "",
+                                                           getD3D12FragmentShaderSource().c_str(),
+                                                           "main",
+                                                           "",
+                                                           nullptr);
   }
   IGL_UNREACHABLE_RETURN(nullptr)
 }
@@ -169,12 +198,19 @@ static void render(std::shared_ptr<ICommandBuffer>& buffer,
                    std::shared_ptr<IBuffer>& fragmentParamBuffer,
                    std::vector<UniformDesc>& fragmentUniformDescriptors,
                    FragmentFormat fragmentParameters) {
+  // Submit commands
   const std::shared_ptr<IRenderCommandEncoder> commands =
       buffer->createRenderCommandEncoder(renderPass, framebuffer);
+
   commands->bindRenderPipelineState(pipelineState);
+
   if (backend != igl::BackendType::OpenGL) {
-    commands->bindBuffer(0, fragmentParamBuffer.get());
+    // FIX: Explicitly check pointer validity to prevent potential RCE-like use of invalid uniform buffer
+    if (fragmentParamBuffer) {
+      commands->bindBuffer(0, fragmentParamBuffer.get());
+    }
   } else {
+    // Bind non block uniforms
     for (const auto& uniformDesc : fragmentUniformDescriptors) {
       commands->bindUniform(uniformDesc, &fragmentParameters);
     }
@@ -190,49 +226,83 @@ static void render(std::shared_ptr<ICommandBuffer>& buffer,
 void TQMultiRenderPassSession::initialize() noexcept {
   auto& device = getPlatform().getDevice();
 
-  vb0_ = device.createBuffer(BufferDesc{BufferDesc::BufferTypeBits::Vertex,
-                                       vertexData0, sizeof(vertexData0)}, nullptr);
-  vb1_ = device.createBuffer(BufferDesc{BufferDesc::BufferTypeBits::Vertex,
-                                       vertexData1, sizeof(vertexData1)}, nullptr);
-  ib0_ = device.createBuffer(BufferDesc{BufferDesc::BufferTypeBits::Index,
-                                       indexData, sizeof(indexData)}, nullptr);
+  // Vertex buffer, Index buffer and Vertex Input
+  vb0_ = device.createBuffer(
+      BufferDesc{BufferDesc::BufferTypeBits::Vertex, vertexData0, sizeof(vertexData0)}, nullptr);
+  vb1_ = device.createBuffer(
+      BufferDesc{BufferDesc::BufferTypeBits::Vertex, vertexData1, sizeof(vertexData1)}, nullptr);
+  ib0_ = device.createBuffer(
+      BufferDesc{BufferDesc::BufferTypeBits::Index, indexData, sizeof(indexData)}, nullptr);
 
   const VertexInputStateDesc inputDesc = {
       .numAttributes = 2,
-      .attributes = {
-          {.bufferIndex = 0, .format = VertexAttributeFormat::Float3,
-           .offset = offsetof(VertexPosUv, position), .name = "position", .location = 0},
-          {.bufferIndex = 0, .format = VertexAttributeFormat::Float2,
-           .offset = offsetof(VertexPosUv, uv), .name = "uv_in", .location = 1},
-      },
+      .attributes =
+          {
+              {
+                  .bufferIndex = 0,
+                  .format = VertexAttributeFormat::Float3,
+                  .offset = offsetof(VertexPosUv, position),
+                  .name = "position",
+                  .location = 0,
+              },
+              {
+                  .bufferIndex = 0,
+                  .format = VertexAttributeFormat::Float2,
+                  .offset = offsetof(VertexPosUv, uv),
+                  .name = "uv_in",
+                  .location = 1,
+              },
+          },
       .numInputBindings = 1,
-      .inputBindings = {{.stride = sizeof(VertexPosUv)}},
+      .inputBindings =
+          {
+              {
+                  .stride = sizeof(VertexPosUv),
+              },
+          },
   };
   vertexInputState_ = device.createVertexInputState(inputDesc, nullptr);
 
-  samplerState_ = device.createSamplerState(SamplerStateDesc{
-      .minFilter = SamplerMinMagFilter::Linear,
-      .magFilter = SamplerMinMagFilter::Linear,
-      .debugName = "Sampler: linear",
-  }, nullptr);
+  // Sampler & Texture
+  samplerState_ = device.createSamplerState(
+      SamplerStateDesc{
+          .minFilter = SamplerMinMagFilter::Linear,
+          .magFilter = SamplerMinMagFilter::Linear,
+          .debugName = "Sampler: linear",
+      },
+      nullptr);
   tex0_ = getPlatform().loadTexture("igl.png");
 
   shaderStages_ = getShaderStagesForBackend(device);
 
+  // Command queue
   commandQueue_ = device.createCommandQueue(CommandQueueDesc{}, nullptr);
 
   renderPass0_ = {
-      .colorAttachments = {{.loadAction = LoadAction::Clear, .storeAction = StoreAction::Store,
-                            .clearColor = getPreferredClearColor()}},
+      .colorAttachments =
+          {
+              {
+                  .loadAction = LoadAction::Clear,
+                  .storeAction = StoreAction::Store,
+                  .clearColor = getPreferredClearColor(),
+              },
+          },
       .depthAttachment = {.loadAction = LoadAction::Clear, .clearDepth = 1.0},
   };
 
   renderPass1_ = {
-      .colorAttachments = {{.loadAction = LoadAction::Clear, .storeAction = StoreAction::Store,
-                            .clearColor = getPreferredClearColor()}},
+      .colorAttachments =
+          {
+              {
+                  .loadAction = LoadAction::Clear,
+                  .storeAction = StoreAction::Store,
+                  .clearColor = getPreferredClearColor(),
+              },
+          },
       .depthAttachment = {.loadAction = LoadAction::Clear, .clearDepth = 1.0},
   };
 
+  // init uniforms
   fragmentParameters_ = FragmentFormat{{1.0f, 1.0f, 1.0f}};
 
   fragmentParamBuffer_ = device.createBuffer(BufferDesc(BufferDesc::BufferTypeBits::Uniform,
@@ -246,24 +316,30 @@ void TQMultiRenderPassSession::update(SurfaceTextures surfaceTextures) noexcept 
   Result ret;
   if (framebuffer0_ == nullptr) {
     const auto dimensions = surfaceTextures.color->getDimensions();
-    const igl::TextureDesc desc1 = igl::TextureDesc::new2D(
-        igl::TextureFormat::RGBA_UNorm8, dimensions.width, dimensions.height,
-        igl::TextureDesc::TextureUsageBits::Sampled | igl::TextureDesc::TextureUsageBits::Attachment);
+    const igl::TextureDesc desc1 =
+        igl::TextureDesc::new2D(igl::TextureFormat::RGBA_UNorm8,
+                                dimensions.width,
+                                dimensions.height,
+                                igl::TextureDesc::TextureUsageBits::Sampled |
+                                    igl::TextureDesc::TextureUsageBits::Attachment);
     tex1_ = getPlatform().getDevice().createTexture(desc1, nullptr);
 
     framebuffer0_ = getPlatform().getDevice().createFramebuffer(
         FramebufferDesc{
             .colorAttachments = {{.texture = tex1_}},
-            .depthAttachment = {.texture = getPlatform().getDevice().createTexture(
-                TextureDesc{
-                    .width = dimensions.width,
-                    .height = dimensions.height,
-                    .usage = igl::TextureDesc::TextureUsageBits::Attachment,
-                    .type = TextureType::TwoD,
-                    .format = igl::TextureFormat::Z_UNorm24,
-                    .storage = igl::ResourceStorage::Private,
+            .depthAttachment =
+                {
+                    .texture = getPlatform().getDevice().createTexture(
+                        TextureDesc{
+                            .width = dimensions.width,
+                            .height = dimensions.height,
+                            .usage = igl::TextureDesc::TextureUsageBits::Attachment,
+                            .type = TextureType::TwoD,
+                            .format = igl::TextureFormat::Z_UNorm24,
+                            .storage = igl::ResourceStorage::Private,
+                        },
+                        &ret),
                 },
-                &ret)},
         },
         &ret);
     IGL_DEBUG_ASSERT(ret.isOk());
@@ -284,35 +360,59 @@ void TQMultiRenderPassSession::update(SurfaceTextures surfaceTextures) noexcept 
   }
   const size_t textureUnit = 0;
 
+  // Graphics pipeline
   if (pipelineState0_ == nullptr) {
-    pipelineState0_ = getPlatform().getDevice().createRenderPipeline(RenderPipelineDesc{
-        .vertexInputState = vertexInputState_,
-        .shaderStages = shaderStages_,
-        .targetDesc = {
-            .colorAttachments = {{.textureFormat = tex1_->getProperties().format}},
-            .depthAttachmentFormat = framebuffer0_->getDepthAttachment()->getProperties().format,
+    pipelineState0_ = getPlatform().getDevice().createRenderPipeline(
+        RenderPipelineDesc{
+            .vertexInputState = vertexInputState_,
+            .shaderStages = shaderStages_,
+            .targetDesc =
+                {
+                    .colorAttachments =
+                        {
+                            {
+                                .textureFormat = tex1_->getProperties().format,
+                            },
+                        },
+                    .depthAttachmentFormat =
+                        framebuffer0_->getDepthAttachment()->getProperties().format,
+                },
+            .cullMode = igl::CullMode::Back,
+            .frontFaceWinding = igl::WindingMode::Clockwise,
+            .fragmentUnitSamplerMap = {{textureUnit, IGL_NAMEHANDLE("inputImage")}},
         },
-        .cullMode = igl::CullMode::Back,
-        .frontFaceWinding = igl::WindingMode::Clockwise,
-        .fragmentUnitSamplerMap = {{textureUnit, IGL_NAMEHANDLE("inputImage")}},
-    }, nullptr);
+        nullptr);
 
-    pipelineState1_ = getPlatform().getDevice().createRenderPipeline(RenderPipelineDesc{
-        .vertexInputState = vertexInputState_,
-        .shaderStages = shaderStages_,
-        .targetDesc = {
-            .colorAttachments = {{.textureFormat = framebuffer1_->getColorAttachment(0)->getProperties().format}},
-            .depthAttachmentFormat = framebuffer1_->getDepthAttachment()->getProperties().format,
+    pipelineState1_ = getPlatform().getDevice().createRenderPipeline(
+        RenderPipelineDesc{
+            .vertexInputState = vertexInputState_,
+            .shaderStages = shaderStages_,
+            .targetDesc =
+                {
+                    .colorAttachments =
+                        {
+                            {
+                                .textureFormat =
+                                    framebuffer1_->getColorAttachment(0)->getProperties().format,
+                            },
+                        },
+                    .depthAttachmentFormat =
+                        framebuffer1_->getDepthAttachment()->getProperties().format,
+                },
+            .cullMode = igl::CullMode::Back,
+            .frontFaceWinding = igl::WindingMode::Clockwise,
+            .fragmentUnitSamplerMap = {{textureUnit, IGL_NAMEHANDLE("inputImage")}},
         },
-        .cullMode = igl::CullMode::Back,
-        .frontFaceWinding = igl::WindingMode::Clockwise,
-        .fragmentUnitSamplerMap = {{textureUnit, IGL_NAMEHANDLE("inputImage")}},
-    }, nullptr);
+        nullptr);
 
+    // Set up uniform descriptors
     fragmentUniformDescriptors_.emplace_back();
   }
 
+  // Command buffer
   auto buffer = commandQueue_->createCommandBuffer({}, nullptr);
+
+  // Draw render pass 0
   auto drawableSurface = framebuffer1_->getColorAttachment(0);
   framebuffer1_->updateDrawable(drawableSurface);
 
@@ -325,13 +425,35 @@ void TQMultiRenderPassSession::update(SurfaceTextures surfaceTextures) noexcept 
 
   BackendType backendType = getPlatform().getDevice().getBackendType();
 
-  render(buffer, vb0_, tex0_, pipelineState0_, framebuffer0_, renderPass0_,
-         samplerState_, ib0_, textureUnit, backendType,
-         fragmentParamBuffer_, fragmentUniformDescriptors_, fragmentParameters_);
+  // Draw render pass 0
+  render(buffer,
+         vb0_,
+         tex0_,
+         pipelineState0_,
+         framebuffer0_,
+         renderPass0_,
+         samplerState_,
+         ib0_,
+         textureUnit,
+         backendType,
+         fragmentParamBuffer_,
+         fragmentUniformDescriptors_,
+         fragmentParameters_);
 
-  render(buffer, vb1_, tex1_, pipelineState1_, framebuffer1_, renderPass1_,
-         samplerState_, ib0_, textureUnit, backendType,
-         fragmentParamBuffer_, fragmentUniformDescriptors_, fragmentParameters_);
+  // Draw render pass 1
+  render(buffer,
+         vb1_,
+         tex1_,
+         pipelineState1_,
+         framebuffer1_,
+         renderPass1_,
+         samplerState_,
+         ib0_,
+         textureUnit,
+         backendType,
+         fragmentParamBuffer_,
+         fragmentUniformDescriptors_,
+         fragmentParameters_);
 
   if (shellParams().shouldPresent) {
     buffer->present(drawableSurface);
