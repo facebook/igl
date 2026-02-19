fb64c97f053b3b13809bf24f9d4b170efd04dcd1
generatedunixname25815638124788833 <noreply+25815638124788833@fb.com>
2026-02-19 00:02:22 -0800

    xplat/graphics/igl/public/src/igl/opengl/glx [B] [B] [A]
    Reviewed By: dmannemela
    
    Differential Revision: D93615366
    
    fbshipit-source-id: d7af44a56fd64767da1d8f2bc69ccf8326ef9631

--- src/igl/opengl/glx/HWDevice.h
+++ src/igl/opengl/glx/HWDevice.h
@@ -11,6 +11,7 @@
 
 namespace igl::opengl::glx {
 
+// @fb-only
 class HWDevice final : public ::igl::opengl::HWDevice {
  public:
   ///--------------------------------------
@@ -25,8 +26,8 @@ class HWDevice final : public ::igl::opengl::HWDevice {
   std::unique_ptr<IContext> createOffscreenContext(size_t width,
                                                    size_t height,
                                                    Result* outResult) const;
-  std::unique_ptr<opengl::Device> createWithContext(std::unique_ptr<IContext> context,
-                                                    Result* outResult) const override;
+  std::unique_ptr<Device> createWithContext(std::unique_ptr<IContext> context,
+                                            Result* outResult) const override;
 };
 
 } // namespace igl::opengl::glx
