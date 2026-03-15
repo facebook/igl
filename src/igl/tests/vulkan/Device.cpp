2e2caa0771bd9e2b7b6bf1f7f90cd391be888f15
Sergey Kosarevsky <sergkosarevsky@meta.com>
2026-03-15 01:14:02 -0700

    igl | vulkan | Refactor `BufferDesc` to use C++20 designated initializers in `vulkan/Device.cpp`
    Reviewed By: RodrigoCastiel
    
    Differential Revision: D96434631
    
    fbshipit-source-id: 3d889c444619380dd55bcafe8afff8e7256d537d

--- src/igl/tests/vulkan/Device.cpp
+++ src/igl/tests/vulkan/Device.cpp
@@ -279,7 +279,12 @@ TEST_F(DeviceVulkanTest, BufferDeviceAddress) {
   Result ret;
 
   auto buffer = iglDev_->createBuffer(
-      BufferDesc{BufferDesc::BufferTypeBits::Uniform, nullptr, 256, ResourceStorage::Shared}, &ret);
+      BufferDesc{
+          .type = BufferDesc::BufferTypeBits::Uniform,
+          .length = 256,
+          .storage = ResourceStorage::Shared,
+      },
+      &ret);
 
   ASSERT_TRUE(ret.isOk()) << ret.message.c_str();
   ASSERT_NE(buffer, nullptr);
