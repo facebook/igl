0eadfe388ace3df66b51e96d4833dc0667324eab
Alexey Medvedev <amedvedev@meta.com>
2026-05-11 13:39:21 -0700

    igl | imgui | Fix enum constant naming convention in KeyCodeTranslator
    Reviewed By: corporateshark
    
    Differential Revision: D104673722
    
    fbshipit-source-id: eba2426c4c10c23c63c9ff68235c0b04c6af933e

--- IGLU/imgui/KeyCodeTranslator.cpp
+++ IGLU/imgui/KeyCodeTranslator.cpp
@@ -14,59 +14,59 @@
 #if IGL_PLATFORM_APPLE
 namespace {
 enum KeyModifiers {
-  kVK_Return = 0x24,
-  kVK_Tab = 0x30,
-  kVK_Delete = 0x33,
-  kVK_Escape = 0x35,
-  kVK_Shift = 0x38,
-  kVK_Option = 0x3A,
-  kVK_Control = 0x3B,
-  kVK_RightArrow = 0x7C,
-  kVK_LeftArrow = 0x7B,
-  kVK_DownArrow = 0x7D,
-  kVK_UpArrow = 0x7E,
-  kVK_ForwardDelete = 0x75,
-  kVK_Home = 0x73,
-  kVK_End = 0x77,
-  kVK_PageUp = 0x74,
-  kVK_PageDown = 0x79,
+  kVkReturn = 0x24,
+  kVkTab = 0x30,
+  kVkDelete = 0x33,
+  kVkEscape = 0x35,
+  kVkShift = 0x38,
+  kVkOption = 0x3A,
+  kVkControl = 0x3B,
+  kVkRightArrow = 0x7C,
+  kVkLeftArrow = 0x7B,
+  kVkDownArrow = 0x7D,
+  kVkUpArrow = 0x7E,
+  kVkForwardDelete = 0x75,
+  kVkHome = 0x73,
+  kVkEnd = 0x77,
+  kVkPageUp = 0x74,
+  kVkPageDown = 0x79,
 };
 
 [[maybe_unused]] ImGuiKey keyFromShellKeyEventApple(igl::shell::KeyEvent event) {
   int keyCode = event.key;
 
   switch (keyCode) {
-  case kVK_Return:
+  case kVkReturn:
     return ImGuiKey_Enter;
-  case kVK_Tab:
+  case kVkTab:
     return ImGuiKey_Tab;
-  case kVK_Delete:
+  case kVkDelete:
     return ImGuiKey_Backspace;
-  case kVK_ForwardDelete:
+  case kVkForwardDelete:
     return ImGuiKey_Delete;
-  case kVK_Escape:
+  case kVkEscape:
     return ImGuiKey_Escape;
-  case kVK_Shift:
+  case kVkShift:
     return ImGuiKey_LeftShift;
-  case kVK_Option:
+  case kVkOption:
     return ImGuiKey_LeftAlt;
-  case kVK_Control:
+  case kVkControl:
     return ImGuiKey_LeftCtrl;
-  case kVK_LeftArrow:
+  case kVkLeftArrow:
     return ImGuiKey_LeftArrow;
-  case kVK_RightArrow:
+  case kVkRightArrow:
     return ImGuiKey_RightArrow;
-  case kVK_UpArrow:
+  case kVkUpArrow:
     return ImGuiKey_UpArrow;
-  case kVK_DownArrow:
+  case kVkDownArrow:
     return ImGuiKey_DownArrow;
-  case kVK_Home:
+  case kVkHome:
     return ImGuiKey_Home;
-  case kVK_End:
+  case kVkEnd:
     return ImGuiKey_End;
-  case kVK_PageUp:
+  case kVkPageUp:
     return ImGuiKey_PageUp;
-  case kVK_PageDown:
+  case kVkPageDown:
     return ImGuiKey_PageDown;
   default:
     return ImGuiKey_None;
