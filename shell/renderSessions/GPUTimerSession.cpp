ac34c7fdb887e6f4b1adf16b031efe5d62187f6d
Sergey Kosarevsky <sergkosarevsky@meta.com>
2026-05-11 14:47:38 -0700

    igl | Guard against null textures.color in GPUTimerSession update()
    Reviewed By: nlutsenko
    
    Differential Revision: D104581143
    
    fbshipit-source-id: 0f02c2a3de2396c4443134d54de401079fd30cf7

--- shell/renderSessions/GPUTimerSession.cpp
+++ shell/renderSessions/GPUTimerSession.cpp
@@ -231,6 +231,11 @@ void GPUTimerSession::initialize() noexcept {
 }
 
 void GPUTimerSession::update(SurfaceTextures textures) noexcept {
+  // Per IGL guidelines, textures.color may be null on some platforms
+  // before the surface is ready (e.g., during window resize on Android/iOS).
+  if (!textures.color) {
+    return;
+  }
   Result ret;
   if (framebuffer_ == nullptr) {
     framebuffer_ = getPlatform().getDevice().createFramebuffer(
