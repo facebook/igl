9833d129e7cb993203745ba0f367ea764e8c6516
Sergey Kosarevsky <sergkosarevsky@meta.com>
2026-05-13 00:02:26 -0700

    igl | iglu | Pass &result and check isOk() in ParametricVertexData::Quad
    Reviewed By: rudybear
    
    Differential Revision: D104741986
    
    fbshipit-source-id: 0c545125872ec3c636fb9b39d890428168b21c5d

--- IGLU/simple_renderer/ParametricVertexData.cpp
+++ IGLU/simple_renderer/ParametricVertexData.cpp
@@ -69,16 +69,28 @@ std::shared_ptr<VertexData> create(igl::IDevice& device,
                                .length = sizeof(uint16_t) * indexData.size()};
 
   const igl::VertexInputStateDesc inputDesc = inputStateDesc();
+  // Per IGL Error Handling rule #24, every resource creation call must pass a
+  // Result* and check it; passing nullptr silently swallows errors.
+  igl::Result result;
   const std::shared_ptr<igl::IVertexInputState> vertexInput =
-      device.createVertexInputState(inputDesc, nullptr);
+      device.createVertexInputState(inputDesc, &result);
+  IGL_DEBUG_ASSERT(result.isOk(), "createVertexInputState() failed: %s", result.message.c_str());
+  IGL_DEBUG_ASSERT(vertexInput != nullptr);
 
   PrimitiveDesc primitiveDesc;
   primitiveDesc.numEntries = sizeof(indexData) / sizeof(indexData[0]);
 
+  auto vb = device.createBuffer(vbDesc, &result);
+  IGL_DEBUG_ASSERT(result.isOk(), "createBuffer(vertex) failed: %s", result.message.c_str());
+  IGL_DEBUG_ASSERT(vb != nullptr);
+  auto ib = device.createBuffer(ibDesc, &result);
+  IGL_DEBUG_ASSERT(result.isOk(), "createBuffer(index) failed: %s", result.message.c_str());
+  IGL_DEBUG_ASSERT(ib != nullptr);
+
   std::shared_ptr<VertexData> vertData =
       std::make_shared<VertexData>(vertexInput,
-                                   device.createBuffer(vbDesc, nullptr),
-                                   device.createBuffer(ibDesc, nullptr),
+                                   std::move(vb),
+                                   std::move(ib),
                                    igl::IndexFormat::UInt16,
                                    primitiveDesc,
                                    igl::PrimitiveType::TriangleStrip);
