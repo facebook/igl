dffa80577266e6bffa7cf7eaaa5e45f71cdb349b
Sergey Kosarevsky <sergkosarevsky@meta.com>
2026-05-18 16:30:55 -0700

    igl | ScissorTestSession: add const to command-buffer local in update()
    Reviewed By: rudybear
    
    Differential Revision: D105532448
    
    fbshipit-source-id: 578c3ac06b02dc9ad5932f770e47c4e54baf5c7a

--- shell/renderSessions/ScissorTestSession.cpp
+++ shell/renderSessions/ScissorTestSession.cpp
@@ -319,7 +319,7 @@ void ScissorTestSession::update(SurfaceTextures textures) noexcept {
   }
 
   // Command Buffers
-  auto buffer = commandQueue_->createCommandBuffer(CommandBufferDesc{}, nullptr);
+  const auto buffer = commandQueue_->createCommandBuffer(CommandBufferDesc{}, nullptr);
   IGL_DEBUG_ASSERT(buffer != nullptr);
   auto drawableSurface = framebuffer_->getColorAttachment(0);
 
