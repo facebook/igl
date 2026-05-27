191dd72dde2d53530a81043aaaf56dc3f4df33dd
Sergey Kosarevsky <sergkosarevsky@meta.com>
2026-05-27 15:31:27 -0700

    igl | BufferMappingSession: Simplify CommandQueueDesc{} to {} at createCommandQueue call site
    Reviewed By: rudybear
    
    Differential Revision: D106556820
    
    fbshipit-source-id: ee0bc5cdae28fcb19ed3217eeca237abee6b9a5a

--- shell/renderSessions/BufferMappingSession.cpp
+++ shell/renderSessions/BufferMappingSession.cpp
@@ -204,7 +204,7 @@ void BufferMappingSession::initialize() noexcept {
   IGL_DEBUG_ASSERT(shaderStages_ != nullptr);
 
   // Command queue
-  commandQueue_ = device.createCommandQueue(CommandQueueDesc{}, nullptr);
+  commandQueue_ = device.createCommandQueue({}, nullptr);
   IGL_DEBUG_ASSERT(commandQueue_ != nullptr);
 
   renderPass_ = {
