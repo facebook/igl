046d2dd6f8a18d083bfc4430d34e86356a8f3831
Sergey Kosarevsky <sergkosarevsky@meta.com>
2026-05-27 16:35:50 -0700

    igl | CopyOperationsSession: Simplify CommandQueueDesc{} to {} at createCommandQueue call site
    Reviewed By: rudybear
    
    Differential Revision: D106556842
    
    fbshipit-source-id: f1d01dbd0f6af505cb3a091147042fc58404c66a

--- shell/renderSessions/CopyOperationsSession.cpp
+++ shell/renderSessions/CopyOperationsSession.cpp
@@ -214,7 +214,7 @@ void CopyOperationsSession::initialize() noexcept {
   IGL_DEBUG_ASSERT(shaderStages_ != nullptr);
 
   // Command queue
-  commandQueue_ = device.createCommandQueue(CommandQueueDesc{}, nullptr);
+  commandQueue_ = device.createCommandQueue({}, nullptr);
   IGL_DEBUG_ASSERT(commandQueue_ != nullptr);
 
   renderPass_ = {
