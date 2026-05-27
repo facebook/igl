a226560481f8439b08242b433c992f99ae2d25a4
Sergey Kosarevsky <sergkosarevsky@meta.com>
2026-05-27 16:35:50 -0700

    igl | DepthBiasSession: Simplify CommandQueueDesc{} to {} at createCommandQueue call site
    Reviewed By: rudybear
    
    Differential Revision: D106556822
    
    fbshipit-source-id: abbfad785bdbc0496ffc72e219e14ff188236afd

--- shell/renderSessions/DepthBiasSession.cpp
+++ shell/renderSessions/DepthBiasSession.cpp
@@ -480,7 +480,7 @@ void DepthBiasSession::initialize() noexcept {
   IGL_DEBUG_ASSERT(shaderStages_ != nullptr);
 
   // Command queue
-  commandQueue_ = device.createCommandQueue(CommandQueueDesc{}, nullptr);
+  commandQueue_ = device.createCommandQueue({}, nullptr);
   IGL_DEBUG_ASSERT(commandQueue_ != nullptr);
 
   // Depth stencil state: depth testing and depth writes enabled
