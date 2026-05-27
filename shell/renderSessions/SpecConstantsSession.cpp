5d44a6e62c99de11159ace487c0cb80ff25d8462
Sergey Kosarevsky <sergkosarevsky@meta.com>
2026-05-27 16:35:50 -0700

    igl | SpecConstantsSession: Simplify CommandQueueDesc{} to {} at createCommandQueue call site
    Reviewed By: rudybear
    
    Differential Revision: D106556829
    
    fbshipit-source-id: be68fd557a50a034317442dcb8c751a5636c8c2c

--- shell/renderSessions/SpecConstantsSession.cpp
+++ shell/renderSessions/SpecConstantsSession.cpp
@@ -262,7 +262,7 @@ void SpecConstantsSession::initialize() noexcept {
   IGL_DEBUG_ASSERT(shaderStages_ != nullptr);
 
   // Command queue
-  commandQueue_ = device.createCommandQueue(CommandQueueDesc{}, nullptr);
+  commandQueue_ = device.createCommandQueue({}, nullptr);
   IGL_DEBUG_ASSERT(commandQueue_ != nullptr);
 
   renderPass_ = {
