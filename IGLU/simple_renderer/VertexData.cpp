a9c1ae7a4ed615be867f08c7eefca71c4a2e8622
Alexey Medvedev <amedvedev@meta.com>
2026-07-31 12:47:01 -0700

    igl | Add `static_assert` for `PrimitiveDesc`
    Reviewed By: corporateshark
    
    Differential Revision: D112685706
    
    fbshipit-source-id: 0c91767771b33ed6cbf40bc02b07d3aa5e15d24c

--- IGLU/simple_renderer/VertexData.cpp
+++ IGLU/simple_renderer/VertexData.cpp
@@ -9,7 +9,10 @@
 
 #include "VertexData.h"
 
+#include <type_traits>
 #include <utility>
+
+static_assert(std::is_trivially_copyable_v<iglu::vertexdata::PrimitiveDesc>);
 
 namespace iglu::vertexdata {
 
