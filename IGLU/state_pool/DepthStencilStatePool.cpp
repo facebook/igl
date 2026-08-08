882c86e45207bb1dfbaaceb47b52b955fd2f648e
Sergey Kosarevsky <sergkosarevsky@meta.com>
2026-08-07 19:20:48 -0700

    igl | IGLU | Add Tracy profiler markers to state_pool/DepthStencilStatePool
    Reviewed By: rudybear
    
    Differential Revision: D115179573
    
    fbshipit-source-id: c711b86c38db2259e1c2911ea9b3f5b227511d07

--- IGLU/state_pool/DepthStencilStatePool.cpp
+++ IGLU/state_pool/DepthStencilStatePool.cpp
@@ -8,6 +8,7 @@
 #include <IGLU/state_pool/DepthStencilStatePool.h>
 
 #include <igl/Device.h>
+#include <igl/Macros.h>
 
 namespace iglu::state_pool {
 
@@ -15,6 +16,7 @@ std::shared_ptr<igl::IDepthStencilState> DepthStencilStatePool::createStateObjec
     igl::IDevice& dev,
     const igl::DepthStencilStateDesc& desc,
     igl::Result* outResult) {
+  IGL_PROFILER_FUNCTION_COLOR(IGL_PROFILER_COLOR_CREATE);
   return dev.createDepthStencilState(desc, outResult);
 }
 
