71bbf61e2e441470bb2c23e010b85551ce551ae2
Alexey Medvedev <amedvedev@meta.com>
2026-08-18 05:05:53 -0700

    igl | Add compile-time type safety assertion for VertexInputBinding
    Differential Revision: D112706031
    
    fbshipit-source-id: db2744d7872cada21c3b285bc669f582120c1d8c

--- src/igl/VertexInputState.cpp
+++ src/igl/VertexInputState.cpp
@@ -13,6 +13,8 @@ static_assert(std::is_trivially_copyable_v<igl::VertexInputBinding>);
 
 using namespace igl;
 
+static_assert(std::is_trivially_copyable_v<igl::VertexInputBinding>);
+
 size_t VertexInputStateDesc::sizeForVertexAttributeFormat(VertexAttributeFormat format) {
   switch (format) {
   case VertexAttributeFormat::Float1:
