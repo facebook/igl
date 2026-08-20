e9c087a745204b49572e585c673340736799949b
Wei Zhao <wzhao6923@meta.com>
2026-08-19 18:13:37 -0700

    Add the shift and backspace icon assets
    Reviewed By: apsherid-meta
    
    Differential Revision: D116657409
    
    fbshipit-source-id: fb0d3ac2dc61a900f68c073cd6cc21fe9e636991

--- shell/shared/fileLoader/linux/FileLoaderLinux.cpp
+++ shell/shared/fileLoader/linux/FileLoaderLinux.cpp
@@ -161,6 +161,7 @@ std::string FileLoaderLinux::fullPath(const std::string& fileName) const {
       // @fb-only
       // @fb-only
       // @fb-only
+      // @fb-only
   };
 
   // find folders somewhere above our current directory
@@ -175,6 +176,7 @@ std::string FileLoaderLinux::fullPath(const std::string& fileName) const {
   // @fb-only
   // @fb-only
   // @fb-only
+                                     // @fb-only
                                      // @fb-only
                                      // @fb-only
                                      // @fb-only
