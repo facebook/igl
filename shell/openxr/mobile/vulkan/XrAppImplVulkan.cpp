76dfbbcd291ce8f0c04c8297733ca510c84a777f
Sergey Kosarevsky <sergkosarevsky@meta.com>
2026-09-21 23:34:36 -0700

    igl | Add trailing `()` to `xrCreateSession()` in the Vulkan app impl
    Differential Revision: D121073961
    
    fbshipit-source-id: 311cadfa854f0dfac5a7e0c69b5739e324aa4c16

--- shell/openxr/mobile/vulkan/XrAppImplVulkan.cpp
+++ shell/openxr/mobile/vulkan/XrAppImplVulkan.cpp
@@ -42,8 +42,9 @@ std::vector<const char*> XrAppImplVulkan::getXrOptionalExtensions() const {
 
 std::unique_ptr<IDevice> XrAppImplVulkan::initIGL(XrInstance instance, XrSystemId systemId) {
   // Get the API requirements.
-  // XR_ERROR_GRAPHICS_REQUIREMENTS_CALL_MISSING is returned on calls to xrCreateSession
-  // if this function has not been called for the instance and systemId before xrCreateSession.
+  // XR_ERROR_GRAPHICS_REQUIREMENTS_CALL_MISSING is returned on calls to xrCreateSession()
+  // if this function has not been called for the instance and systemId before
+  // xrCreateSession().
   PFN_xrGetVulkanGraphicsRequirementsKHR pfnGetVulkanGraphicsRequirementsKHR = nullptr;
   XR_CHECK(xrGetInstanceProcAddr(
       instance,
