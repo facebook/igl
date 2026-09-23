779af76f8285d8c296b024ce5f9bd3a4651d1de8
Sergey Kosarevsky <sergkosarevsky@meta.com>
2026-09-23 12:05:47 -0700

    igl | Use designated initializers for the descriptors in `TextureArrayFloat` tests
    Reviewed By: MagnificRogue
    
    Differential Revision: D121371402
    
    fbshipit-source-id: a78bc44452c8149f17c560879d11879ba3b01feb

--- src/igl/tests/TextureArrayFloat.cpp
+++ src/igl/tests/TextureArrayFloat.cpp
@@ -126,10 +126,11 @@ class TextureArrayFloatTest : public ::testing::Test {
     ASSERT_TRUE(framebuffer_ != nullptr);
 
     // Initialize render pass descriptor
-    renderPass_.colorAttachments.resize(1);
-    renderPass_.colorAttachments[0].loadAction = LoadAction::Clear;
-    renderPass_.colorAttachments[0].storeAction = StoreAction::Store;
-    renderPass_.colorAttachments[0].clearColor = {0.0, 0.0, 0.0, 1.0};
+    renderPass_ = {
+        .colorAttachments = {{.loadAction = LoadAction::Clear,
+                              .storeAction = StoreAction::Store,
+                              .clearColor = {0.0, 0.0, 0.0, 1.0}}},
+    };
     // Initialize shader stages
     std::unique_ptr<IShaderStages> stages;
     if (iglDev_->getBackendType() == BackendType::OpenGL) {
@@ -251,14 +252,13 @@ class TextureArrayFloatTest : public ::testing::Test {
 
     // Initialize Graphics Pipeline Descriptor, but leave the creation
     // to the individual tests in case further customization is required
-    renderPipelineDesc_.vertexInputState = vertexInputState_;
-    renderPipelineDesc_.shaderStages = shaderStages_;
-    renderPipelineDesc_.targetDesc.colorAttachments.resize(1);
-    renderPipelineDesc_.targetDesc.colorAttachments[0].textureFormat =
-        offscreenTexture_->getFormat();
-    renderPipelineDesc_.fragmentUnitSamplerMap[textureUnit_] =
-        IGL_NAMEHANDLE(data::shader::kSimpleSampler);
-    renderPipelineDesc_.cullMode = igl::CullMode::Disabled;
+    renderPipelineDesc_ = {
+        .vertexInputState = vertexInputState_,
+        .shaderStages = shaderStages_,
+        .targetDesc = {.colorAttachments = {{.textureFormat = offscreenTexture_->getFormat()}}},
+        .cullMode = igl::CullMode::Disabled,
+        .fragmentUnitSamplerMap = {{textureUnit_, IGL_NAMEHANDLE(data::shader::kSimpleSampler)}},
+    };
   }
 
   void TearDown() override {}
