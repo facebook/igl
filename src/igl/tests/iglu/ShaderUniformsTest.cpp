2e768a5fb1fc62882058b17f157e66ca9a1f7112
Sergey Kosarevsky <sergkosarevsky@meta.com>
2026-09-24 11:47:29 -0700

    igl | Use a designated initializer for the vertex input state in `ShaderUniformsTest`
    Reviewed By: maliarov
    
    Differential Revision: D121580492
    
    fbshipit-source-id: 8068790b4d47c6aa74e42e2b5b30e006a3b28e74

--- src/igl/tests/iglu/ShaderUniformsTest.cpp
+++ src/igl/tests/iglu/ShaderUniformsTest.cpp
@@ -331,8 +331,7 @@ TEST_F(ShaderUniformsTest, BindUploadsSuballocatedUniformBuffer) {
   ASSERT_TRUE(ret.isOk()) << ret.message;
   ASSERT_TRUE(offscreen != nullptr);
 
-  FramebufferDesc framebufferDesc;
-  framebufferDesc.colorAttachments[0].texture = offscreen;
+  const FramebufferDesc framebufferDesc{.colorAttachments = {{.texture = offscreen}}};
   std::shared_ptr<IFramebuffer> framebuffer = iglDev_->createFramebuffer(framebufferDesc, &ret);
   ASSERT_TRUE(ret.isOk()) << ret.message;
   ASSERT_TRUE(framebuffer != nullptr);
@@ -348,20 +347,22 @@ TEST_F(ShaderUniformsTest, BindUploadsSuballocatedUniformBuffer) {
   util::createSimpleShaderStages(iglDev_, stages);
   std::shared_ptr<IShaderStages> shaderStages = std::move(stages);
 
-  VertexInputStateDesc inputDesc;
-  inputDesc.attributes[0].format = VertexAttributeFormat::Float4;
-  inputDesc.attributes[0].offset = 0;
-  inputDesc.attributes[0].bufferIndex = data::shader::kSimplePosIndex;
-  inputDesc.attributes[0].name = data::shader::kSimplePos;
-  inputDesc.attributes[0].location = 0;
-  inputDesc.inputBindings[0].stride = sizeof(float) * 4;
-  inputDesc.attributes[1].format = VertexAttributeFormat::Float2;
-  inputDesc.attributes[1].offset = 0;
-  inputDesc.attributes[1].bufferIndex = data::shader::kSimpleUvIndex;
-  inputDesc.attributes[1].name = data::shader::kSimpleUv;
-  inputDesc.attributes[1].location = 1;
-  inputDesc.inputBindings[1].stride = sizeof(float) * 2;
-  inputDesc.numAttributes = inputDesc.numInputBindings = 2;
+  // numAttributes has to equal numInputBindings when using more than one buffer
+  const VertexInputStateDesc inputDesc{
+      .numAttributes = 2,
+      .attributes = {{.bufferIndex = data::shader::kSimplePosIndex,
+                      .format = VertexAttributeFormat::Float4,
+                      .offset = 0,
+                      .name = std::string(data::shader::kSimplePos),
+                      .location = 0},
+                     {.bufferIndex = data::shader::kSimpleUvIndex,
+                      .format = VertexAttributeFormat::Float2,
+                      .offset = 0,
+                      .name = std::string(data::shader::kSimpleUv),
+                      .location = 1}},
+      .numInputBindings = 2,
+      .inputBindings = {{.stride = sizeof(float) * 4}, {.stride = sizeof(float) * 2}},
+  };
   std::shared_ptr<IVertexInputState> vertexInputState =
       iglDev_->createVertexInputState(inputDesc, &ret);
   ASSERT_TRUE(ret.isOk()) << ret.message;
