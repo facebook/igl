a03145b91c459a5bb7bf919a4f7da611bc45e77b
Alexey Medvedev <amedvedev@meta.com>
2026-09-24 11:42:17 -0700

    igl | Suppress `facebook-static-object-destructor-check` in `Jni.cpp` and `Type.h`
    Reviewed By: boguscoder
    
    Differential Revision: D119890134
    
    fbshipit-source-id: a8d8404bd15be7d8f3c45585cd18ee26e63c0bf3

--- shell/android/jni/Jni.cpp
+++ shell/android/jni/Jni.cpp
@@ -75,9 +75,10 @@ std::optional<BackendVersion> activeBackendVersion;
 // argv-style `args` Intent extra (see extractArgsExtra() below) and stash the
 // strings here so the char* pointers handed to initializeCommandLineArgs()
 // stay valid for the lifetime of the process.
-// NOLINTNEXTLINE(facebook-static-object-destructor-check)
+// NOLINTBEGIN(facebook-static-object-destructor-check)
 std::vector<std::string> gPlatformArgvStorage;
 std::vector<char*> gPlatformArgv;
+// NOLINTEND(facebook-static-object-destructor-check)
 constexpr const char* kPlatformArgv0 = "igl_android_shell";
 
 // Single Intent extra whose value (a whitespace-separated string) is forwarded
