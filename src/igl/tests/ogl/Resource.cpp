f2e76abc22b4905fe8e169ac0ca8d5668b07934f
Sergey Kosarevsky <sergkosarevsky@meta.com>
2026-09-24 11:47:29 -0700

    igl | opengl | Use a designated initializer for the vertex input state in `Resource` tests
    Reviewed By: maliarov
    
    Differential Revision: D121580514
    
    fbshipit-source-id: fa98f0746f009d8156872600c24460135ddb3678

--- src/igl/tests/ogl/Resource.cpp
+++ src/igl/tests/ogl/Resource.cpp
@@ -37,21 +37,21 @@ class ResourceOGLTest : public ::testing::Test {
     shaderStages_ = std::move(stages);
 
     // Initialize input to vertex shader
-    VertexInputStateDesc inputDesc;
-
-    inputDesc.attributes[0].format = VertexAttributeFormat::Float4;
-    inputDesc.attributes[0].offset = 0;
-    inputDesc.attributes[0].bufferIndex = data::shader::kSimplePosIndex;
-    inputDesc.attributes[0].name = data::shader::kSimplePos;
-    inputDesc.attributes[0].location = 0;
-    inputDesc.inputBindings[0].stride = sizeof(float) * 4;
-
-    inputDesc.attributes[1].format = VertexAttributeFormat::Float2;
-    inputDesc.attributes[1].offset = 0;
-    inputDesc.attributes[1].bufferIndex = data::shader::kSimpleUvIndex;
-    inputDesc.attributes[1].name = data::shader::kSimpleUv;
-    inputDesc.attributes[1].location = 1;
-    inputDesc.inputBindings[1].stride = sizeof(float) * 2;
+    // numAttributes / numInputBindings are deliberately left at their defaults here, exactly as
+    // before this rewrite.
+    const VertexInputStateDesc inputDesc{
+        .attributes = {{.bufferIndex = data::shader::kSimplePosIndex,
+                        .format = VertexAttributeFormat::Float4,
+                        .offset = 0,
+                        .name = std::string(data::shader::kSimplePos),
+                        .location = 0},
+                       {.bufferIndex = data::shader::kSimpleUvIndex,
+                        .format = VertexAttributeFormat::Float2,
+                        .offset = 0,
+                        .name = std::string(data::shader::kSimpleUv),
+                        .location = 1}},
+        .inputBindings = {{.stride = sizeof(float) * 4}, {.stride = sizeof(float) * 2}},
+    };
 
     vertexInputState_ = iglDev_->createVertexInputState(inputDesc, &ret);
     ASSERT_EQ(ret.code, Result::Code::Ok);
